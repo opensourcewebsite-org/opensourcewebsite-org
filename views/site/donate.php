c6eaaecdaf751fcc44d1cbfa8d78be7bd25758ae
Vitaliy S <pro2847@gmail.com>
2018-08-04 17:52:35 +0700

    Donate page

--- views/site/donate.php
+++ views/site/donate.php
@@ -9,7 +9,18 @@ $this->title = 'Donate';
 <div class="site-contact">
     <h1><?= Html::encode($this->title) ?></h1>
 
+<p>
+    OpenSourceWebsite is entirely supported by the general public.
+</p>
 
-Coming soon
+<p>
+Your donations increase your social rating on this Website. Your donations pay for staff, servers, bandwidth, maintenance, development and protective infrastructure.
+</p>
+
+<h4>We accept cryptocurrencies</h4>
+
+<p>
+    For questions and suggestions, please <a href="/contact">contact</a> us.
+</p>
 
 </div>
