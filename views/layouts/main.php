c05a9fa6a93e6997070c318e51dbb1ed597fa505
Vitaliy S <pro2847@gmail.com>
2018-08-10 17:53:15 +0700

    Fix career link

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -71,10 +71,10 @@ AppAsset::register($this);
 <footer class="footer">
     <div class="container">
         <?= Html::a(Yii::t('app', 'Donate'), ['site/donate']) ?> |
+        <?= Html::a(Yii::t('app', 'Career'), ['site/team']) ?> |
         <?= Html::a(Yii::t('app', 'Code repository '), 'https://gitlab.com/opensourcewebsite-org/opensourcewebsite-org') ?> |
         <?= Html::a(Yii::t('app', 'Issues'), 'https://gitlab.com/opensourcewebsite-org/opensourcewebsite-org/issues') ?> |
         <?= Html::a(Yii::t('app', 'Wiki'), 'https://gitlab.com/opensourcewebsite-org/opensourcewebsite-org/wikis/home') ?> |
-        <?= Html::a(Yii::t('app', 'Team'), ['site/team']) ?> |
         <?= Html::a(Yii::t('app', 'Terms of Use'), ['site/terms-of-use']) ?> |
         <?= Html::a(Yii::t('app', 'Privacy Policy'), ['site/privacy-policy']) ?> |
         <?= Html::a(Yii::t('app', 'Contact'), ['site/contact']) ?> |
