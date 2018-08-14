567294793313e33ffb808b27039067ce143b1aa5
Ilya Butorin <rolmonk@gmail.com>
2018-08-14 13:43:28 +0700

    docker

--- config/db.dist.php
+++ config/db.dist.php
@@ -2,7 +2,7 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
+    'dsn' => 'mysql:host=localhost;dbname=osw',
     'username' => 'root',
     'password' => '',
     'charset' => 'utf8',
