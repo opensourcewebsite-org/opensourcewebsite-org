5d2ead86ecff21750137c9160bc68f7eb2b28ca0
Ilia Butorin <rolmonk@gmail.com>
2018-08-20 17:14:55 +0700

    travis (#8)

---
+++ config/test_db.travis.php
@@ -0,0 +1,8 @@
+<?php
+$db = require __DIR__ . '/db.php';
+// test database! Important not to run tests on production or development databases
+$db['dsn'] = 'mysql:host=localhost;dbname=test';
+$db['user'] = 'root';
+$db['password'] = '';
+
+return $db;
