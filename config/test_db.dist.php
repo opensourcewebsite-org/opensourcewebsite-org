b49b19ff4c85f18b8b16a8509b7f770d9e39f359
Vitaliy S <pro2847@gmail.com>
2018-08-20 17:32:25 +0700

    Fix travis

--- config/test_db.dist.php
+++ config/test_db.dist.php
@@ -1,6 +1,6 @@
 <?php
 $db = require __DIR__ . '/db.dist.php';
 // test database! Important not to run tests on production or development databases
-$db['dsn'] = 'mysql:host=localhost;dbname=yii2_basic_tests';
+$db['dsn'] = 'mysql:host=localhost;dbname=test';
 
 return $db;
