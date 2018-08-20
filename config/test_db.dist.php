cb52d73e69fca1667b8b2aa4b38d4b519a130905
Ilya Butorin <rolmonk@gmail.com>
2018-08-20 18:49:58 +0700

    envs

--- config/test_db.dist.php
+++
@@ -1,8 +0,0 @@
-<?php
-$db = require __DIR__ . '/db.dist.php';
-// test database! Important not to run tests on production or development databases
-$db['dsn'] = 'mysql:host=localhost;dbname=test';
-$db['username'] = 'root';
-$db['password'] = '';
-
-return $db;
---
+++ config/test_db.php
@@ -0,0 +1,2 @@
+<?php
+return require __DIR__ . '/db.php';
