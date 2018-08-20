5f4774769be611e7d3407d669ef57807dffcc3c0
Vitaliy S <pro2847@gmail.com>
2018-08-20 17:59:01 +0700

    Fix travis

--- config/test_db.dist.php
+++ config/test_db.dist.php
@@ -2,5 +2,7 @@
 $db = require __DIR__ . '/db.dist.php';
 // test database! Important not to run tests on production or development databases
 $db['dsn'] = 'mysql:host=localhost;dbname=test';
+$db['username'] = 'root';
+$db['password'] = '';
 
 return $db;
