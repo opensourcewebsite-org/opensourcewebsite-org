d2860e9d8d920e35c9d885b387c6d0f4fb5f1407
Ilia Butorin <rolmonk@gmail.com>
2018-08-21 14:22:26 +0700

    config fix (#11)
    * travis
    
    * envs
    
    * config fix
    
    * test config
    
    * fixtures
    
    * configs
    
    * configs
    
    * configs
    
    * tests
    
    * tests
    
    * config

--- web/index.php
+++ web/index.php
@@ -4,14 +4,15 @@ use Dotenv\Dotenv;
 
 $config = include '../config/web-local.php';
 
+require __DIR__ . '/../vendor/autoload.php';
+
+$dotenv = new Dotenv('..');
+$dotenv->load();
+
 defined('YII_DEBUG') or define('YII_DEBUG', getenv('YII_DEBUG'));
 defined('YII_ENV') or define('YII_ENV', getenv('YII_ENV') ?: 'prod');
 
-require __DIR__ . '/../vendor/autoload.php';
 require __DIR__ . '/../vendor/yiisoft/yii2/Yii.php';
-
-$dotenv = new Dotenv(dir(__DIR__));
-$dotenv->load();
 
 Yii::$classMap['yii\helpers\Html'] = '@app/components/Html.php';
 
