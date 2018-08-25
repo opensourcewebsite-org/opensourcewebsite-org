91e5d3bf71476bd3780f4309f9361b42cb4f7345
HenonoaH <heno@pearlthoughts.com>
2018-08-25 22:48:43 +0530

    display registered user and confirmed user count in info box

---
+++ controllers/UserController.php
@@ -0,0 +1,58 @@
+<?php
+namespace app\controllers;
+
+use Yii;
+use app\models\User;
+use yii\web\Controller;
+use yii\filters\AccessControl;
+
+class UserController extends Controller
+{
+
+    /**
+     * {@inheritdoc}
+     */
+    public function behaviors()
+    {
+        return [
+            'access' => [
+                'class' => AccessControl::className(),
+                'only' => ['display'],
+                'rules' => [
+                        [
+                        'allow' => true,
+                        'roles' => ['@'],
+                    ],
+                ],
+            ],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function actions()
+    {
+        return [
+            'error' => [
+                'class' => 'yii\web\ErrorAction',
+            ],
+        ];
+    }
+
+    /**
+     * Lists all User models.
+     * @return mixed
+     */
+    public function actionDisplay()
+    {
+        $registered_users = User::find()->all();
+        $confirmed_users = User::findAll(['is_email_confirmed' => true]);
+
+        return $this->render('display', [
+            'registered_users' => count($registered_users),
+            'confirmed_users' => count($confirmed_users),
+            'percentage' => (count($confirmed_users) / count($registered_users)) * 100,
+        ]);
+    }
+}
