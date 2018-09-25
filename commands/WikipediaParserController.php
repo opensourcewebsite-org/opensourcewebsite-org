9b8b19bdac8ba15768861b42e7ad5607c5e9ce39
HenonoaH <heno@pearlthoughts.com>
2018-09-25 18:26:02 +0530

    adds wikipedia command

---
+++ commands/WikipediaParserController.php
@@ -0,0 +1,215 @@
+<?php
+
+namespace app\commands;
+
+use Yii;
+use yii\db\Query;
+use yii\helpers\Console;
+use app\models\WikiPage;
+use yii\httpclient\Client;
+use yii\console\Controller;
+use app\models\WikiLanguage;
+use yii\base\ErrorException;
+use yii\helpers\ArrayHelper;
+use app\models\UserWikiToken;
+use app\components\WikiParser;
+use yii\web\ServerErrorHttpException;
+
+/**
+ *
+ * @property integer $groupId
+ */
+class WikipediaParserController extends Controller
+{
+
+    const PARSE_INTERVAL = 60;
+    const UPDATE_INTERVAL = 24 * 3600;
+    const UPDATE_ACTIVE_USER_TIMEOUT = 60 * 10;
+    const PAGE_PARSE_RETRY_INTERVAL = 5;
+    const PAGE_PARSE_RETRY_COUNT = 3;
+
+    public function options($actionID)
+    {
+        return array_merge(parent::options($actionID), [
+            'log',
+        ]);
+    }
+
+    public function actionIndex()
+    {
+        while (true) {
+            $this->log('Running watchlists parser...');
+            $this->processPages();
+            $this->log('Running languages parser...');
+            $this->parse();
+            sleep(self::PARSE_INTERVAL);
+        }
+    }
+
+    protected function parse()
+    {
+        $baseUrl = 'https://wikidata.org/w';
+        $client = new Client([
+            'baseUrl' => $baseUrl,
+        ]);
+        $languageCodes = WikiLanguage::find()
+            ->select('id')
+            ->indexBy('code')
+            ->column();
+        $baseQuery = WikiPage::find()->select('id');
+        if ($page = WikiPage::find()
+            ->with('language')
+            ->joinWith('users')
+            ->where([
+                'or',
+                    ['wiki_page.updated_at' => null],
+                    ['<', 'wiki_page.updated_at', time() - self::UPDATE_INTERVAL],
+            ])
+            ->andWhere(['is not', 'user.id', null])
+            ->one()) {
+            $this->log("Parsing page: {$page->title}");
+            for ($retry = 0; $retry <= self::PAGE_PARSE_RETRY_COUNT; $retry++) {
+                try {
+                    $response = $client->get('api.php', [
+                            'action' => 'wbgetentities',
+                            'format' => 'json',
+                            'props' => 'sitelinks',
+                            'utf8' => 1,
+                            'normalize' => 1,
+                            'sites' => "{$page->language->code}wiki",
+                            'titles' => $page->title,
+                        ])->send();
+                    break;
+                } catch (ErrorException $e) {
+                    $this->log('Error parsing page ' . $page->title . ' - ' . $e->getMessage());
+                    if ($retry == self::PAGE_PARSE_RETRY_COUNT) {
+                        $page->updateAttributes(['group_id' => $this->getGroupId(), 'updated_at' => time()]);
+                    } else {
+                        sleep(self::PAGE_PARSE_RETRY_INTERVAL);
+                    }
+                }
+            }
+            if (isset($response)) {
+                $data = $response->data;
+                if ($data['success'] == 1 && count($data['entities'])) {
+                    $result = array_shift($data['entities']);
+                    if (isset($result['sitelinks'])) {
+                        $query = clone $baseQuery;
+                        $query->where(['id' => $page->id]);
+                        $sitelinkData = [];
+                        foreach ($result['sitelinks'] as $sitelink) {
+                            if (substr($sitelink['site'], -4, 4) == 'wiki') {
+                                $pos = strpos($sitelink['site'], 'wiki');
+                                $language = substr($sitelink['site'], 0, $pos);
+                                $languageId = ArrayHelper::getValue($languageCodes, $language);
+                                if ($languageId && $languageId != $page->language_id) {
+                                    $sitelinkData[$languageId] = $sitelink['title'];
+                                    $query->orWhere([
+                                        'and', ['language_id' => $languageId], ['title' => $sitelink['title']],
+                                    ]);
+                                }
+                            }
+                        }
+                        $groupQuery = clone $query;
+                        $groupQuery->andWhere(['is not', 'group_id', null]);
+                        $groupId = $groupQuery->select('group_id')->scalar();
+                        if (!$groupId) {
+                            $groupId = $this->getGroupId();
+                        }
+                        $pageIds = $query->column();
+                        $time = time();
+                        WikiPage::updateAll(['group_id' => $groupId, 'updated_at' => $time], ['id' => $pageIds]);
+                        $existingLanguageIds = WikiPage::find()
+                            ->select('language_id')
+                            ->where(['id' => $pageIds])
+                            ->column();
+                        $missingLanguageIds = array_diff(array_keys($sitelinkData), $existingLanguageIds);
+                        $rows = [];
+                        foreach ($missingLanguageIds as $missingLanguageId) {
+                            $rows[] = [
+                                $missingLanguageId,
+                                0,
+                                $sitelinkData[$missingLanguageId],
+                                $groupId,
+                                $time,
+                            ];
+                        }
+                        Yii::$app->db->createCommand()->batchInsert(
+                            '{{%wiki_page}}', ['language_id', 'ns', 'title', 'group_id', 'updated_at'], $rows
+                        )->execute();
+                    } else {
+                        $page->updateAttributes(['group_id' => $this->getGroupId(), 'updated_at' => time()]);
+                    }
+                } else {
+                    $page->updateAttributes(['group_id' => $this->getGroupId(), 'updated_at' => time()]);
+                }
+            }
+        }
+        if (WikiPage::find()
+                ->joinWith('users')
+                ->where([
+                    'or',
+                        ['wiki_page.updated_at' => null],
+                        ['<', 'wiki_page.updated_at', time() - self::UPDATE_INTERVAL],
+                ])
+                ->andWhere(['is not', 'user.id', null])
+                ->exists()) {
+            $this->parse();
+        } else {
+            return true;
+        }
+    }
+
+    protected function getGroupId()
+    {
+        return WikiPage::find()->max('group_id') + 1;
+    }
+
+    protected function processPages()
+    {
+        $tokens = UserWikiToken::find()
+            ->andWhere([
+                'or',
+                    ['updated_at' => null],
+                    ['<', 'updated_at', time() - self::UPDATE_INTERVAL],
+                    [
+                    'exists', (new Query())
+                    ->select('*')
+                    ->from('user')
+                    ->where('user.id = user_wiki_token.user_id')
+                    ->andWhere(['>', 'user_wiki_token.updated_at', time() - self::UPDATE_ACTIVE_USER_TIMEOUT]),
+                ],
+            ])
+            ->andWhere(['!=', 'status', UserWikiToken::STATUS_HAS_ERROR])
+            ->all();
+        $counter = count($tokens);
+        $this->log("Found $counter tokens to update");
+        foreach ($tokens as $token) {
+            $this->updatePages($token);
+        }
+    }
+
+    protected function updatePages(UserWikiToken $token)
+    {
+        $parser = new WikiParser([
+            'user_id' => $token->user_id,
+            'language_id' => $token->language_id,
+        ]);
+        try {
+            $parser->run();
+        } catch (ServerErrorHttpException $e) {
+            Yii::$app->db->createCommand()->update(
+                '{{%user_wiki_token}}', ['status' => UserWikiToken::STATUS_HAS_ERROR], ['user_id' => $token->user_id, 'language_id' => $token->language_id]
+            )->execute();
+        }
+        $this->log("Updated token #{$token->id}");
+        Yii::$app->db->createCommand()->update(
+            '{{%user_wiki_token}}', ['updated_at' => time()], ['id' => $token->id]
+        )->execute();
+    }
+
+    protected function log($message)
+    {
+        Console::output($message, Console::FG_GREEN, Console::BOLD);
+    }
+}
