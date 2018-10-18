ef8a0de78c56b30e6709071f93137b1c0ca65bbe
Vitaliy S <pro2847@gmail.com>
2018-10-18 17:06:05 +0700

    Fix moqup view bug

--- views/moqup/design-view.php
+++ views/moqup/design-view.php
@@ -24,14 +24,14 @@ if (!empty($moqup)):
                 <?= Html::a(Html::tag('i', '', ['class' => 'fa fa-code-branch'])
                     . Html::tag('span', $moqup->forksNumber, ['class' => 'badge badge-light ml-1']),
                     ['moqup/design-edit', 'fork' => $moqup->id], [
-                        'class' => 'btn btn-light', 
+                        'class' => 'btn btn-light',
                         'title' => 'Fork Page'
                     ]) ?>
 
                 <?= Html::a(Html::tag('i', '', ['class' => 'fa fa-star' . ($followed ? ' text-primary' : '')])
                     . Html::tag('span', $moqup->followersNumber, ['class' => 'badge badge-light ml-1']),
                     [($followed ? 'user/unfollow-moqup' : 'user/follow-moqup'), 'id' => $moqup->id], [
-                        'class' => 'btn btn-light ' . ($followed ? 'unfollow-page' : 'follow-page'), 
+                        'class' => 'btn btn-light ' . ($followed ? 'unfollow-page' : 'follow-page'),
                         'title' => $followed ? 'Unfollow Page' : 'Follow Page',
                     ]) ?>
             </div>
@@ -57,7 +57,7 @@ if (!empty($moqup)):
                 <div class="col-md-12">
                     <div class="tab-content">
                         <div class="tab-pane active" id="view">
-                            <iframe id="prev-frame" src="<?= yii::$app->urlManager->createUrl(['moqup/design-preview']) ?>" frameborder="0" sandbox="allow-forms allow-popups allow-same-origin allow-scripts allow-pointer-lock" class="col-md-12"></iframe>
+                            <?= $moqup->html ?>
                         </div>
                         <div class="tab-pane" id="html">
                             <div id="html-editor" class="ace-editor"><?= Html::encode($moqup->html) ?></div>
@@ -100,7 +100,7 @@ $("#prev-frame").css("min-height", prevHeight + "px");
 $(".follow-page, .unfollow-page, .follow-user, .unfollow-user").on("click", function(event) {
     event.preventDefault();
     var url = $(this).attr("href");
-    
+
     $.post(url, {}, function(result) {
         if (result == "1") {
             location.reload();
