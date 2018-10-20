34886da16aafd95913a05167643f85412a6a64ed
Komal <lath.komal@gmail.com>
2018-10-20 17:43:43 +0530

    Create a separate Database for tests (osw_test) and add configurations in test_db.php

--- config/test_db.php
+++ config/test_db.php
@@ -1,2 +1,20 @@
 <?php
-return require __DIR__ . '/db.php';
+
+$username = getenv('DB_USERNAME') ?: 'root';
+$password = getenv('DB_PASSWORD') ?: 'root';
+$name = getenv('DB_NAME') ?: 'osw_test';
+$host = getenv('DB_HOST') ?: 'localhost';
+$port = getenv('DB_PORT') ?: '3306';
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => "mysql:host=$host;port=$port;dbname=$name",
+    'username' => $username,
+    'password' => $password,
+    'charset' => 'utf8',
+
+    // Schema cache options (for production environment)
+    //'enableSchemaCache' => true,
+    //'schemaCacheDuration' => 60,
+    //'schemaCache' => 'cache',
+];
