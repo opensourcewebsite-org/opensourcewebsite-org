f85012301296c70a954b381b2350a3e5d524039b
HenonoaH <heno@pearlthoughts.com>
2018-11-07 23:00:01 +0530

    redirect to account page if user did not confirm email

--- controllers/MoqupController.php
+++ controllers/MoqupController.php
@@ -33,9 +33,6 @@ class MoqupController extends Controller
                     [
                         'allow' => true,
                         'roles' => ['@'],
-                        'matchCallback' => function ($rule, $action) {
-                            return Yii::$app->user->identity->is_email_confirmed;
-                        }
                     ],
                 ],
             ],
