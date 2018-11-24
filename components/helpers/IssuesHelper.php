0ccd8854b419424a9cf4cfc971d6d54ef625dd1c
HenonoaH <heno@pearlthoughts.com>
2018-11-24 18:25:46 +0530

    adds upgrade migration for user vote and remove neutral constant

--- components/helpers/IssuesHelper.php
+++ components/helpers/IssuesHelper.php
@@ -17,16 +17,13 @@ class IssuesHelper extends Component
         $votes = $issue->getUserVotesPercent(false);
 
         $v1 = $votes[UserIssueVote::YES];
-        $v2 = $votes[UserIssueVote::NEUTRAL];
         $v3 = $votes[UserIssueVote::NO];
 
         $vote1 = !empty($v1) ? Converter::formatNumber($v1, 0) . '%' : '';
-        $vote2 = !empty($v2) ? Converter::formatNumber($v2, 0) . '%' : '';
         $vote3 = !empty($v3) ? Converter::formatNumber($v3, 0) . '%' : '';
 
         return '<div class="progress" style="height: 20px;">
             <div class="progress-bar bg-success" role="progressbar" style="width: ' . $v1 . '%" aria-valuenow="' . $v1 . '" aria-valuemin="0" aria-valuemax="100">' . $vote1 . '</div>
-            <div class="progress-bar bg-light" role="progressbar" style="width: ' . $v2 . '%" aria-valuenow="' . $v2 . '" aria-valuemin="0" aria-valuemax="100">' . $vote2 . '</div>
             <div class="progress-bar bg-danger" role="progressbar" style="width: ' . $v3 . '%" aria-valuenow="' . $v3 . '" aria-valuemin="0" aria-valuemax="100">' . $vote3 . '</div>
         </div>';
     }
