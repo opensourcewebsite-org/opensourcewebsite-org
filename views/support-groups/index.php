b9e1067e4fa741d45f5c062b9876ec62245d220c
illyar80 <illyar80@gmail.com>
2018-12-05 15:03:33 +0200

    view commands (support groups)

--- views/support-groups/index.php
+++ views/support-groups/index.php
@@ -16,55 +16,65 @@ $this->params['breadcrumbs'][] = $this->title;
             <?= Html::a('New Support Group', ['create'], ['class' => 'btn btn-success ml-3']) ?>
         </div>
         <div class="card-body p-0">
-            <div class="table table-hover">
-                <?= GridView::widget([
-                    'dataProvider' => $dataProvider,
-                    'summary' => false,
-                    'columns' => [
-                        'title',
-                        [
-                            'class' => 'yii\grid\ActionColumn',
-                            'template' => '{all}',
-                            'buttons' => [
-                                'all' => function ($url, $model, $key) {
-                                    return ButtonDropdown::widget([
-                                        'encodeLabel' => false, // if you're going to use html on the button label
-                                        'label' => '<i class="fas fa-cog"></i>',
-                                        'dropdown' => [
-                                            //'encodeLabels' => false, // if you're going to use html on the items' labels
-                                            'items' => [
-                                                [
-                                                    'label' => Yii::t('app', 'Members'),
-                                                    'url' => ['members', 'id' => $key],
-                                                    'linkOptions' => ['class' => 'dropdown-item']
-                                                ],
-                                                [
-                                                    'label' => Yii::t('app', 'Telegram bots'),
-                                                    'url' => ['bots', 'id' => $key],
-                                                    'linkOptions' => ['class' => 'dropdown-item']
-                                                ],
-                                                [
-                                                    'label' => Yii::t('app', 'Commands'),
-                                                    'url' => ['commands', 'id' => $key],
-                                                    'linkOptions' => ['class' => 'dropdown-item']
-                                                ],
-                                                [
-                                                    'label' => Yii::t('app', 'Edit'),
-                                                    'url' => ['update', 'id' => $key],
-                                                    'linkOptions' => ['class' => 'dropdown-item']
-                                                ],
+            <?= GridView::widget([
+                'dataProvider' => $dataProvider,
+                'summary' => false,
+                'showHeader' => false,
+                'tableOptions' => ['class' => 'table table-hover table-condensed'],
+                'columns' => [
+                    'title',
+                    [
+                        'class' => 'yii\grid\ActionColumn',
+                        'template' => '{all}',
+                        'buttons' => [
+                            'all' => function ($url, $model, $key) {
+                                return ButtonDropdown::widget([
+                                    'encodeLabel' => false, // if you're going to use html on the button label
+                                    'label' => '<i class="fas fa-cog"></i>',
+                                    'dropdown' => [
+                                        //'encodeLabels' => false, // if you're going to use html on the items' labels
+                                        'items' => [
+                                            [
+                                                'label' => Yii::t('app', 'Members'),
+                                                'url' => ['members', 'id' => $key],
+                                                'linkOptions' => ['class' => 'dropdown-item']
+                                            ],
+                                            [
+                                                'label' => Yii::t('app', 'Telegram bots'),
+                                                'url' => ['bots', 'id' => $key],
+                                                'linkOptions' => ['class' => 'dropdown-item']
+                                            ],
+                                            /*[
+                                                'label' => Yii::t('app', 'Commands'),
+                                                'url' => ['commands', 'id' => $key],
+                                                'linkOptions' => ['class' => 'dropdown-item']
+                                            ],*/
+                                            [
+                                                'label' => Yii::t('app', 'Edit'),
+                                                'url' => ['update', 'id' => $key],
+                                                'linkOptions' => ['class' => 'dropdown-item']
                                             ],
                                         ],
-                                        'options' => [
-                                            'class' => 'btn-default',   // btn-success, btn-info, et cetera
-                                        ],
-                                    ]);
-                                },
-                            ],
+                                    ],
+                                    'options' => [
+                                        'class' => 'btn-default',   // btn-success, btn-info, et cetera
+                                    ],
+                                ]);
+                            },
                         ],
                     ],
-                ]); ?>
-            </div>
+                    [
+                        'content' => function ($model) {
+                            return Html::a('commands', ['commands', 'id' => $model->id]);
+                        }
+                    ],
+                    [
+                        'content' => function ($model) {
+                            return Html::a('clients', ['clients', 'id' => $model->id]);
+                        }
+                    ],
+                ],
+            ]); ?>
             <div class="card-footer clearfix">
             </div>
         </div>
