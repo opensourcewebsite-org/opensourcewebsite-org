267a95e75bcb2a9d2afdfe7bb52e52275acdee6a
Egor Mokeev <zetamen@yandex.ru>
2019-02-07 15:15:41 +0300

    Username field is optional (#152)

---
+++ migrations/m190207_115309_set_optional_username_field_user_table.php
@@ -0,0 +1,25 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Class m190207_115309_set_optional_username_field_user_table
+ */
+class m190207_115309_set_optional_username_field_user_table extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->alterColumn('user', 'username', $this->string(255));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        $this->alterColumn('user', 'username', $this->string(255)->notNull());
+    }
+}
