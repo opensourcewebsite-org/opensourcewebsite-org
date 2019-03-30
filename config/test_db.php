12da33982fd7f174351e5078897803e68136ce27
Komal <lath.komal@gmail.com>
2019-03-30 22:55:40 +0530

    Uncomment pageid condition

--- config/test_db.php
+++ config/test_db.php
@@ -1,15 +1,2 @@
 <?php
-
-
-return [
-    'class' => 'yii\db\Connection',
-    'dsn' => "mysql:host=localhost;port=3306;dbname=osw_test",
-    'username' => 'root',
-    'password' => 'root',
-    'charset' => 'utf8',
-
-    // Schema cache options (for production environment)
-    //'enableSchemaCache' => true,
-    //'schemaCacheDuration' => 60,
-    //'schemaCache' => 'cache',
-];
+return require __DIR__ . '/db.php';
