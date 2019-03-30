9c0d5c32729b7936126d584a5e40a317875bc9a2
Komal <lath.komal@gmail.com>
2019-03-30 22:50:26 +0530

    #167, page id uncomment

--- config/test_db.php
+++ config/test_db.php
@@ -1,2 +1,15 @@
 <?php
-return require __DIR__ . '/db.php';
+
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => "mysql:host=localhost;port=3306;dbname=osw_test",
+    'username' => 'root',
+    'password' => 'root',
+    'charset' => 'utf8',
+
+    // Schema cache options (for production environment)
+    //'enableSchemaCache' => true,
+    //'schemaCacheDuration' => 60,
+    //'schemaCache' => 'cache',
+];
