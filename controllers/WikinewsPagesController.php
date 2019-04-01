5f9044b20afcc6ac92933f18d27a92b0caedc00f
Komal Kedia <komalkedia@users.noreply.github.com>
2019-04-01 09:52:28 +0530

    Wikinews (#172)
    * Create a separate Database for tests (osw_test) and add configurations in test_db.php
    
    * Unit test created for Moqup Model
    
    * Formatted file
    
    * Revert test_db
    
    * Restore old test files
    
    * Load all fixtures in first Test 'LoginFormTest'
    Remove load fixtures from 'MoqupTest'
    
    * Restore ContactForm Test
    
    * Fix issue of Forbidden Access in Confirm email after click on verification link
    Refactor Confirm email code
    
    * Tests for Confirm Email and Add rating after confirm email
    
    * Add referral module:
    1. Create new page and a link in user top menu after Account link for Loyality program
    2. For each invited referral, with a confirmed email, charge 1 rating to the user who invited him. If the future user follows the invitation link, then write the code of the referrer to him in the cookie for one year.
    3. One-tier system of referrals.
    4. Unit Test for add referral Bonus
    5. Refactor addRating function in User model
    6. Fix bug in confirm mail to only confirm mail if user is login with correct id as mentioned in verification link
    7. Unit test for confirm mail with in correct login and guest user
    8. Fix top menu position, so that the menu elements can be visible
    
    * Remove conflict duplicated addRating function
    
    * Format files
    
    * Fix bug count referrals only if, email id is confirmed
    
    * Task: remove a field “balance” from table “rating” and change code for correct work without this field
    
    * Show referral page after email id is confirmed
    
    * https://github.com/opensourcewebsite-org/opensourcewebsite-org/issues/167
    
    * https://github.com/opensourcewebsite-org/opensourcewebsite-org/issues/167
    
    * #167,
    page id uncomment
    
    * Uncomment pageid condition

---
+++ controllers/WikinewsPagesController.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace app\controllers;
+
+use Yii;
+use yii\helpers\Url;
+use yii\web\Controller;
+use app\models\WikiLanguage;
+use app\models\UserWikiToken;
+use yii\filters\AccessControl;
+use yii\data\ActiveDataProvider;
+use yii\web\NotFoundHttpException;
+use app\models\search\WikinewsSearch;
+use app\models\WikinewsPage;
+use app\models\WikinewsLanguage;
+
+/**
+ * WikipediaPagesController implements the CRUD actions for WikiPage model.
+ */
+class WikinewsPagesController extends Controller
+{
+
+    public function behaviors()
+    {
+        return [
+            'access' => [
+                'class' => AccessControl::class,
+                'rules' => [
+                    [
+                        'actions' => ['index', 'create'],
+                        'roles' => ['@'],
+                        'allow' => true,
+                    ],
+                ],
+            ],
+        ];
+    }
+
+    public function actionIndex()
+    {
+        Url::remember();
+
+        $searchModel = new WikinewsSearch();
+        $dataProvider = $searchModel->search(Yii::$app->request->queryParams);
+
+        return $this->render('index', [
+            'searchModel' => $searchModel,
+            'dataProvider' => $dataProvider
+        ]);
+    }
+	
+    public function actionCreate()
+    {
+        $model = new WikinewsPage([
+            'created_by' => Yii::$app->user->id,
+            'created_at' => time()
+        ]);
+
+        $languageArray = WikinewsLanguage::find()->all();
+
+        if (Yii::$app->request->isGet && Yii::$app->request->isAjax) {
+            if ($model->load(Yii::$app->request->get()) && $model->validate()) {				
+				preg_match("#https://([a-z]{2}).wikinews.org/wiki/([A-Za-z0-9,_.-]+)$#", $model->url, $matches);
+				if (isset($matches[1])) {
+					$model->language_id = WikinewsLanguage::find()->select('id')->where(['code'=>$matches[1]])->scalar();
+				}
+				if (isset($matches[2])) {
+					$model->title = trim($matches[2]);
+				}
+				$wikiNewsPage = WikinewsPage::find()->where(['language_id'=>$model->language_id, 'title'=>$model->title])->one();
+				if ($wikiNewsPage) {
+					$wikiNewsPage->parsed_at = NULL;
+					$wikiNewsPage->url = $model->url;
+					$model = $wikiNewsPage;
+				}
+				if($model->save()){
+					return $this->redirect(['wikinews-pages/index']);
+				}                
+            }
+        }
+        
+        return $this->renderAjax('form', [
+            'model' => $model,
+            'languageArray' => $languageArray,
+        ]);
+    }
+}
