c7aa9c1cba319c335e5ea249153fcdaf97f026bb
S.M. Riad <smriad@gmail.com>
2019-05-13 10:18:41 +0600

    parser

--- views/wikinews-pages/index.php
+++ views/wikinews-pages/index.php
@@ -62,7 +62,7 @@ $this->title = Yii::t('menu', 'Wikinews pages');
                         'label' => 'Title',
                         'value' => function ($model) {
                             //$link = "https://{$model->language->code}.wikinews.org/?curid=".$model->pageid;//wiki/".$model->title;
-$link = "https://{$model->language->code}.wikinews.org/wiki/".$model->title;
+                        $link = "https://{$model->language->code}.wikinews.org/wiki/" . $model->title;
                             return Html::a($model->title, $link, ['target' => '_blank']);
                         },
                         'format' => 'raw',
