14f8dd387c69ccba3331c1b8c3774865db716b9f
alexa <alexa@alexa-K501LX>
2019-06-15 18:13:19 +0700

    Fix bug for a command /lang.

--- models/BotHandler.php
+++ models/BotHandler.php
@@ -117,9 +117,9 @@ class BotHandler extends BotApi
         ]);
 
         # case: when group has only 1 language
-        $all_languages = SupportGroupLanguage::findAll(['support_group_id' => $this->support_group_id]);
-        if (count($all_languages) == 1) {
-            $this->_language_code = $all_languages[0]->language_code;
+        $languages = $this->getLanguagesByGroup();
+        if (count($languages) == 1) {
+            $this->_language_code = $languages[0]->language_code;
         }
 
         #default language
@@ -248,7 +248,19 @@ class BotHandler extends BotApi
             $supportGroup->save();
 
             return $default_response ? $this->generateDefaultResponse() : true;
-        } elseif (trim($this->getMessage()->getText()) == '/lang' || $this->_language_code == null) {
+        } elseif (trim($this->getMessage()->getText()) == '/lang' || $this->_language_code == null) {               
+            # when group has only 1 language
+            $languages = $this->getLanguagesByGroup();
+            if (count($languages) == 1) {
+                #if command /land setting  send our response  
+                $commands = $this->executeCommand();  
+                if ($commands->command == '/lang') {
+                   return $this->generateResponse($commands->supportGroupCommandTexts);                    
+                }
+                #if command /land not setting  send defult response
+                return $this->generateDefaultResponse();
+            }
+            
             $output = '';
 
             $availableLanguagesName = SupportGroupLanguage::find()
@@ -289,15 +301,19 @@ class BotHandler extends BotApi
                 'supportGroupBot',
                 'supportGroupCommandTexts',
             ])
-            ->one();
-
+            ->one(); 
+         
         if (!$commands) {
             return $this->generateDefaultResponse();
-        }
+        }  
+        
+        if ($commands->command === '/lang') {
+            return $commands; 
+        } 
 
         return $this->generateResponse($commands->supportGroupCommandTexts);
     }
-
+    
     /**
      * @return bool|int
      * @throws \yii\db\Exception
@@ -423,6 +439,14 @@ class BotHandler extends BotApi
         ]);
 
         return $model->save();
+    }
+    
+     /**
+     * @return array
+     */
+    public function getLanguagesByGroup()
+    {
+        return SupportGroupLanguage::findAll(['support_group_id' => $this->support_group_id]);
     }
 
     /**
