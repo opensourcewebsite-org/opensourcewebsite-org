5905eec3b3b45f72dc91fcc90dbe3e17c364ed74
HenonoaH <heno@pearlthoughts.com>
2019-08-07 21:46:20 +0530

    remove debug code

--- config/web.php
+++ config/web.php
@@ -176,7 +176,7 @@ if (YII_ENV_DEV) {
     $config['modules']['debug'] = [
         'class' => 'yii\debug\Module',
         // uncomment the following to add your IP if you are not connecting from localhost.
-        'allowedIPs' => ['*'],
+        //'allowedIPs' => ['127.0.0.1', '::1'],
     ];
 
     $config['bootstrap'][] = 'gii';
