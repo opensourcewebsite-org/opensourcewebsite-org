0121c126b04b64c159ac5989d1411c593fdb2909
Vitaliy Shevchenko <pro2847@gmail.com>
2019-10-28 19:15:10 +0700

    bot texts

--- views/bot/start/index.php
+++ views/bot/start/index.php
@@ -7,18 +7,29 @@
 
 <?= Yii::t('bot', 'Available features (beta testing)'); ?>:
 - <?= Yii::t('bot', 'Jobs. Automatic job and employee search based on different criteria and locations in automatic mode.'); ?>
+
 - <?= Yii::t('bot', 'Ads. Buying, selling, renting, services by different criteria and locations in automatic mode.'); ?>
 
 
 <?= Yii::t('bot', 'Coming soon (under development)'); ?>:
 - <?= Yii::t('bot', 'Real estates. Buying, selling, renting.'); ?>
+
 - <?= Yii::t('bot', 'Dating. Search for new contacts based on different criteria and locations.'); ?>
+
 - <?= Yii::t('bot', 'P2P currency/cryptocurrency exchange between users.'); ?>
+
 - <?= Yii::t('bot', 'Paid surveys from companies for users.'); ?>
+
 - <?= Yii::t('bot', 'Personalized news.'); ?>
+
 - <?= Yii::t('bot', 'Personalized advertising/offers from companies for users.'); ?>
+
 - <?= Yii::t('bot', 'Money bets between users.'); ?>
+
 - <?= Yii::t('bot', 'Anonymous lotteries.'); ?>
+
 - <?= Yii::t('bot', 'Hobby clubs. Search for like-minded people by different criteria and locations.'); ?>
+
 - <?= Yii::t('bot', 'Traveling together. Search for fellow travelers.'); ?>
+
 - <?= Yii::t('bot', 'Informing about important events and interesting events.'); ?>.
