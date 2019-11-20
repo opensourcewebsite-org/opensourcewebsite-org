eb701e23ef6b980ee696e451d22588dd5a397864
Vitaliy Shevchenko <pro2847@gmail.com>
2019-11-20 20:00:19 +0700

    texts

--- modules/bot/views/help/index.php
+++ modules/bot/views/help/index.php
@@ -1,7 +1,6 @@
-<b><?= \Yii::t('bot', 'Available commands:') ?></b><br/>
-/start<br/>
-/my_profile<br/>
-/my_rating<br/>
-/my_language<br/>
-/my_currency<br/>
-/help<br/>
+<b><?= \Yii::t('bot', 'Available commands') ?>:</b><br/>
+/my_profile - <?= \Yii::t('bot', 'Profile') ?><br/>
+/my_rating - <?= \Yii::t('bot', 'Rating') ?><br/>
+/my_language - <?= \Yii::t('bot', 'Language') ?><br/>
+/my_currency - <?= \Yii::t('bot', 'Currency') ?><br/>
+/start - <?= \Yii::t('bot', 'Greeting') ?><br/>
--- modules/bot/views/profile/currency.php
+++ modules/bot/views/profile/currency.php
@@ -7,5 +7,5 @@ if ($currencyModel) { ?>
     <?= $currencyModel->hasErrors() ? \Yii::t('bot', 'Sorry, it looks like something went wrong.') : NULL ?><br/>
 <?php } ?>
 
-<b><?= \Yii::t('bot', 'Your Currency') ?>:</b><br/><br/>
+<b><?= \Yii::t('bot', 'Your Currency') ?></b><br/><br/>
 <?= $currentName ?> (<?= strtoupper($currentCode) ?>)
