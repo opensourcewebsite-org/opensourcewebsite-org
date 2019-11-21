78ac1a7c00a70e7224efc7681c1cfa062b97a461
Vitaliy Shevchenko <pro2847@gmail.com>
2019-11-21 21:56:03 +0700

    texts

--- modules/bot/views/profile/rating.php
+++ modules/bot/views/profile/rating.php
@@ -4,5 +4,9 @@
 <b><?= \Yii::t('bot', 'Overall Rating') ?>:</b> <?= $overall_rating[0] ?>, 0,00% <?= \Yii::t('bot', 'of') ?> <?= $overall_rating[1] ?><br/>
 <b><?= \Yii::t('bot', 'Rank') ?>:</b> #<?= $ranking[0] ?> <?= \Yii::t('bot', 'among') ?> <?= $ranking[1] ?> <?= \Yii::t('bot', 'users') ?><br/><br/>
 
-<?= \Yii::t('bot', 'How does your User Rating work?') ?>: https://opensourcewebsite.org/participation<br/>
-<?= \Yii::t('bot', 'How to increase your User Rating?') ?>: https://opensourcewebsite.org/donate + https://opensourcewebsite.org/team<br/>
+<?= \Yii::t('bot', 'How does your User Rating work?') ?><br/>
+https://opensourcewebsite.org/participation<br/><br/>
+
+<?= \Yii::t('bot', 'How to increase your User Rating?') ?><br/>
+https://opensourcewebsite.org/donate<br/>
+https://opensourcewebsite.org/team
