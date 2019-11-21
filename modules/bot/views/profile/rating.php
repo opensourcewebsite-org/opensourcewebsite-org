eb381d0f4d6519c54cffdf7f132438d7492bccf2
Vitaliy Shevchenko <pro2847@gmail.com>
2019-11-21 21:54:18 +0700

    texts

--- modules/bot/views/profile/rating.php
+++ modules/bot/views/profile/rating.php
@@ -1,6 +1,8 @@
 <b><?= \Yii::t('bot', 'Your Rating') ?></b><br/><br/>
-<b><?= \Yii::t('bot', 'Active Rating') ?>:</b> <?= $active_rating ?><br/>
-<b><?= \Yii::t('bot', 'Overall Rating') ?>:</b> <?= $overall_rating[0] ?>
-<?= \Yii::t('bot', 'of') ?> <?= $overall_rating[1] ?><br/>
-<b><?= \Yii::t('bot', 'Ranking') ?>:</b> #<?= $ranking[0] ?> <?= \Yii::t('bot', 'among') ?>
-<?= $ranking[1] ?> <?= \Yii::t('bot', 'users') ?><br/>
+
+<b><?= \Yii::t('bot', 'Active Rating') ?>:</b> <?= $active_rating ?> (last 30 days)<br/>
+<b><?= \Yii::t('bot', 'Overall Rating') ?>:</b> <?= $overall_rating[0] ?>, 0,00% <?= \Yii::t('bot', 'of') ?> <?= $overall_rating[1] ?><br/>
+<b><?= \Yii::t('bot', 'Rank') ?>:</b> #<?= $ranking[0] ?> <?= \Yii::t('bot', 'among') ?> <?= $ranking[1] ?> <?= \Yii::t('bot', 'users') ?><br/><br/>
+
+<?= \Yii::t('bot', 'How does your User Rating work?') ?>: https://opensourcewebsite.org/participation<br/>
+<?= \Yii::t('bot', 'How to increase your User Rating?') ?>: https://opensourcewebsite.org/donate + https://opensourcewebsite.org/team<br/>
