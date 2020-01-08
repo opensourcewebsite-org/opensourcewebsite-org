e1d3c030bd3e10cf6ca616c430b87cf2ca82c923
Timofey Brushteyn <brushteyntimofey@gmail.com>
2020-01-08 20:04:31 +0500

    fix

---
+++ config/params.dist.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'adminEmail' => 'admin@example.com',
+    'securityEmail' => 'security@example.com',
+    'user.passwordResetTokenExpire' => 3600 * 24, //3600 miliseconds * 24 hours
+    'baseUrl' => 'https://opensourcewebsite.org',
+];
