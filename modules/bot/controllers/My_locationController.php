2e7e3610a087a2c767af70fb2ef1320df60839c6
Vladimir Strizhakov <vladimir.strizhakov@touchcast.com>
2020-02-04 02:00:03 +0200

    /my_location command

--- modules/bot/controllers/My_locationController.php
+++ modules/bot/controllers/My_locationController.php
@@ -3,6 +3,7 @@
 namespace app\modules\bot\controllers;
 
 use app\modules\bot\components\CommandController as Controller;
+use app\modules\bot\components\BotClient;
 
 /**
  * Class My_locationController
@@ -16,6 +17,24 @@ class My_locationController extends Controller
      */
     public function actionIndex()
     {
-        return $this->render('index');
+    	
+
+    	$userId = \Yii::$app->requestMessage->getChat()->getId();
+    	$botClient = \Yii::$app->botClient->getModel();	
+
+    	\Yii::$app->responseMessage->setKeyboard(new \TelegramBot\Api\Types\ReplyKeyboardMarkup([
+    		[
+    			[
+    				'text' => \Yii::t('bot', 'Send Location'),
+    				'request_location' => true
+    			]
+    		]
+    	]));
+
+        return $this->render('index', [
+        	'longtitude' => $botClient->location_lon,
+        	'latitude' => $botClient->location_lat,
+        	'lastUpdate' => $botClient->location_at,
+        ]);
     }
 }
--- modules/bot/views/my_location/index.php
+++ modules/bot/views/my_location/index.php
@@ -1,2 +1,16 @@
-<b><?= \Yii::t('bot', 'Your Location') ?></b><br/><br/>
-Here
+<b><?= \Yii::t('bot', 'Your Location') ?></b>
+<br/><br/>
+<?php
+	if (isset($longtitude))
+	{
+		echo \Yii::t('bot', 'Longtitude') . ': ' . $longtitude . '<br/>';
+	}
+	if (isset($latitude))
+	{
+		echo \Yii::t('bot', 'Latitude') . ': ' . $latitude . '<br/>';
+	}
+	if (isset($lastUpdate))
+	{
+		echo \Yii::t('bot', 'Last update') . ': ' . $lastUpdate . '<br/>';
+	}
+?>
