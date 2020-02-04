e3c6180530fbafe03d105ddea37c68c6c0f5f6bd
Vadim <berinchik2012@gmail.com>
2020-02-04 05:40:20 -0600

    .env.dist

---
+++ config/params.dist.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'adminEmail' => 'admin@example.com',
+    'securityEmail' => 'security@example.com',
+    'user.passwordResetTokenExpire' => 3600 * 24, //3600 miliseconds * 24 hours
+    'baseUrl' => 'https://opensourcewebsite.org', //Used for /commands and Telegram webhooks
+	// 'telegramProxy' => '41.190.95.92:47720,
+];
