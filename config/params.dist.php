e9b6a7d21bd7dd1e2b64ef176f19eb9ee568de23
Vadim <berinchik2012@gmail.com>
2020-02-04 19:48:00 -0600

    Dist files recover

--- config/params.dist.php
+++ config/params.dist.php
@@ -5,5 +5,5 @@ return [
     'securityEmail' => 'security@example.com',
     'user.passwordResetTokenExpire' => 3600 * 24, //3600 miliseconds * 24 hours
     'baseUrl' => 'https://opensourcewebsite.org', //Used for /commands and Telegram webhooks
-	// 'telegramProxy' => '41.190.95.92:47720,
+	//'telegramProxy' => '',
 ];
