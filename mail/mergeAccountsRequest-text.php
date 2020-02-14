221d03162b54223bdfac94c3cf94b25d5080cc75
Vladimir Strizhakov <vladimir.strizhakov@touchcast.com>
2020-02-14 18:35:19 +0200

    Merge accounts email fix

--- mail/mergeAccountsRequest-text.php
+++ mail/mergeAccountsRequest-text.php
@@ -1,4 +1,4 @@
 Hello<?= !empty($user->name) ? ", {$user->name}!" : '!' ?>
 You requested a merge of your user account with <?= !empty($userToMerge->name) ? $userToMerge->name : 'noname' ?>`s account.
-To confirm this request please go by the following link: <?= Yii::$app->urlManager->createAbsoluteUrl(['site/merge-accounts', 'token' => $mergeAccountsToken]) ?>
+To confirm this request please go by the following link: <?= Yii::$app->urlManager->createAbsoluteUrl(['site/merge-accounts', 'token' => $mergeAccountsRequestToken]) ?>
 If you didn't create such request, just ignore this letter.
