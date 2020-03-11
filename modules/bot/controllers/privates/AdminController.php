7d7203bdde553b486822a0589d3cb276f0ceecf7
Vadim <berinchik2012@gmail.com>
2020-03-10 21:32:11 -0600

    Trailing spaces deleted

--- modules/bot/controllers/privates/AdminController.php
+++ modules/bot/controllers/privates/AdminController.php
@@ -23,7 +23,7 @@ class AdminController extends Controller
     public function actionIndex($page = 1)
     {
         $chatQuery = $this->getTelegramUser()->getAdministratedChats();
-     
+
         $buttons = [];
         $currentRow = [];
 
