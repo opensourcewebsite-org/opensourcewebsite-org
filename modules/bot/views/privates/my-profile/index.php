7a989ae1249a87a7da7ad3db35ca226d73a67860
Vladimir Strizhakov <vova_strigh@list.ru>
2020-03-31 12:15:30 +0300

    MyProfile fix (#327)
    Co-authored-by: Vladimir Strizhakov <vladimir.strizhakov@touchcast.com>

--- modules/bot/views/privates/my-profile/index.php
+++ modules/bot/views/privates/my-profile/index.php
@@ -1,11 +1,7 @@
-<?php
 
-use app\models\User;
-?>
 <b><?= Yii::t('bot', 'Your Profile') ?></b><br/>
 <br/>
 <?php
-/** @var \TelegramBot\Api\Types\User $profile */
 if (isset($firstName)) {
     echo Yii::t('bot', 'First Name') . ': ' . $firstName . '<br/>';
 }
@@ -24,24 +20,21 @@ if (isset($birthday)) {
 if (isset($currency)) {
     echo Yii::t('bot', 'Currency') . ': ' .  $currency . '<br/>';
 }
-if (isset($language)) {
+if (isset($interfaceLanguage)) {
     echo Yii::t('bot', 'Interface language') . ': ' .  $interfaceLanguage . '<br/>';
 }
 if (isset($timezone)) {
      echo Yii::t('bot', 'Timezone') . ': ' . $timezone . '<br/>';
 }
-?>
-<? if (!empty($languages)) : ?>
-<br/>
-<?= Yii::t('bot', 'Languages') ?>:<br/>
-<? foreach ($languages as $language) : ?>
-<?= $language ?><br/>
-<? endforeach; ?>
-<? endif; ?>
-<? if (!empty($citizenships)) : ?>
-<br/>
-<?= Yii::t('bot', 'Citizenships') ?>:<br/>
-<? foreach ($citizenships as $citizenship) : ?>
-<?= $citizenship ?><br/>
-<? endforeach; ?>
-<? endif; ?>
+if (!empty($languages)) {
+    echo Yii::t('bot', 'Languages') . '<br/>';
+    foreach ($languages as $language) {
+        echo $language . '<br/>';
+    }
+}
+if (!empty($citizenships)) {
+    echo Yii::t('bot', 'Citizenships') . '<br/>';
+    foreach ($citizenships as $citizenship) {
+        echo $citizenship . '<br/>';
+    }
+}
