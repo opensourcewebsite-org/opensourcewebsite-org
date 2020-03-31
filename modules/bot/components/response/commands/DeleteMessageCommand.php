e4b4f1824bdc044371d309e233032dde4af74618
Vladimir Strizhakov <vladimir.strizhakov@touchcast.com>
2020-03-31 12:38:15 +0300

    DeleteMessage command fix

--- modules/bot/components/response/commands/DeleteMessageCommand.php
+++ modules/bot/components/response/commands/DeleteMessageCommand.php
@@ -1,13 +1,14 @@
 <?php
 namespace app\modules\bot\components\response\commands;
 
-use app\modules\bot\components\response\Command;
 use TelegramBot\Api\BotApi;
 
 class DeleteMessageCommand extends Command
 {
     public function __construct($chatId, $messageId)
     {
+        parent::__construct([]);
+
         $this->chatId = $chatId;
         $this->messageId = $messageId;
     }
