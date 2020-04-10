b1fb6ceea12f47743ba6c03f9a14687f78c75c51
Vladimir Strizhakov <mrhugaa@gmail.com>
2020-04-11 01:03:31 +0300

    Create timezone table. Use 'timezone_id' in user table instead of 'timezone'.

--- modules/bot/controllers/privates/MyProfileController.php
+++ modules/bot/controllers/privates/MyProfileController.php
@@ -2,13 +2,12 @@
 
 namespace app\modules\bot\controllers\privates;
 
+use app\models\UserCitizenship;
+use app\models\UserLanguage;
 use app\modules\bot\components\helpers\Emoji;
 use app\modules\bot\components\response\ResponseBuilder;
 use Yii;
 use app\modules\bot\components\Controller;
-use app\models\Currency;
-use app\models\Language;
-use app\components\helpers\TimeHelper;
 
 /**
  * Class MyProfileController
@@ -24,10 +23,8 @@ class MyProfileController extends Controller
     {
         $telegramUser = $this->getTelegramUser();
         $user = $this->getUser();
-        $timezones = TimeHelper::timezonesList();
 
         $currency = $user->currency;
-        $interfaceLanguage = $telegramUser->language;
 
         $params = [
             'firstName' => $telegramUser->provider_user_first_name,
@@ -37,11 +34,11 @@ class MyProfileController extends Controller
             'sexuality' => isset($user->sexuality) ? $user->sexuality->name : null,
             'birthday' => $user->birthday,
             'currency' => isset($currency) ? "{$currency->name} ({$currency->code})" : null,
-            'timezone' => $timezones[$user->timezone],
-            'languages' => array_map(function ($userLanguage) {
+            'timezone' => '(UTC ' . $user->timezone->getUTCOffset() . ') ' . $user->timezone->location,
+            'languages' => array_map(function (UserLanguage $userLanguage) {
                 return $userLanguage->getDisplayName();
             }, $user->languages),
-            'citizenships' => array_map(function($citizenship) {
+            'citizenships' => array_map(function(UserCitizenship $citizenship) {
                 return $citizenship->country->name;
             }, $user->citizenships),
         ];
