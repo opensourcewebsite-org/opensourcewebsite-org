14b30093030a45128c74747f8f54e6431a4c5624
Andrey Guzich <a.guzich@gmail.com>
2020-04-19 01:50:53 -0400

    --

--- modules/bot/views/publics/voteban/user-kicked.php
+++ modules/bot/views/publics/voteban/user-kicked.php
@@ -1,3 +1,3 @@
-🔫 <?=Yii::t('bot', '{user} has been kicked — the only way to get this user back is for admins to manualy unban in chat settings', ['user'=>$user])?><br/><br/>
-<?=Yii::t('bot', 'Voters who chose to kick')?>:<br/>
-<?=$voters?>
+🔫 <?= Yii::t('bot', '{user} has been kicked — the only way to get this user back is for admins to manualy unban in chat settings', ['user'=>$user])?><br/><br/>
+<?= Yii::t('bot', 'Voters who chose to kick')?>:<br/>
+<?= $voters?>
