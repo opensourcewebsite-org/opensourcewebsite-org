de14db7e1fb8c445c6ee3eba5e199b000d668c84
Andrey Guzich <a.guzich@gmail.com>
2020-04-28 05:01:34 -0400

    --

--- modules/bot/views/privates/admin-chat-refresh/index.php
+++ modules/bot/views/privates/admin-chat-refresh/index.php
@@ -1,1 +1,1 @@
-<?= Yii::t('bot', 'Group settings updated') ?>. <?= Yii::t('bot', 'Now group administrators can manage this group using my features') ?>.
+<?= Yii::t('bot', 'The list of group administrators updated'); ?>.
