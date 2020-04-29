906d6d89b78b4c6d597052091ea1060205ea201f
Vitaliy Shevchenko <pro2847@gmail.com>
2020-04-29 12:30:14 +0700

    bot texts

--- modules/bot/views/privates/admin-vote-ban/index.php
+++ modules/bot/views/privates/admin-vote-ban/index.php
@@ -2,4 +2,4 @@
 <br/>
 <?= Yii::t('bot', 'Vote Ban') ?> - <?= Yii::t('bot', 'allows members to vote for a specific member to be banned or kicked from the group') ?>.<br/>
 <br/>
-<?= Yii::t('bot', 'To start a vote, send a reply “/voteban” to a message of any member of the group') ?>. <?= Yii::t('bot', 'Administrators cannot be banned') ?>.
+<?= Yii::t('bot', 'To start a vote, send a reply “/voteban” to a message of any member of the group') ?>. <?= Yii::t('bot', 'Not applicable for administrators') ?>.
