64b6b5995a599d93d98766c7612d6c8b4e29fb77
Andrey Guzich <aguzich@gmail.com>
2020-05-05 21:21:15 +0300

    --

--- actions/ErrorAction.php
+++ actions/ErrorAction.php
@@ -20,7 +20,7 @@ class ErrorAction extends \yii\web\ErrorAction
         $customResult = null;
 
         if ($this->isCookiesDisabledReason()) {
-            $customResult = $this->controller->render('error/cookies-disabled',$this->getViewRenderParams());
+            $customResult = $this->controller->render('error/cookies-disabled', $this->getViewRenderParams());
         }
 
         return isset($customResult) ? $customResult : $this->controller->render($this->view ?: $this->id, $this->getViewRenderParams());
@@ -36,8 +36,8 @@ class ErrorAction extends \yii\web\ErrorAction
 
         $badRequestException = new BadRequestHttpException();
         $badRequestExceptionCode = $badRequestException->statusCode;
-        if ( $this->getExceptionCode() == $badRequestExceptionCode) {
-            if ( $this->controller->enableCsrfValidation && Yii::$app->request->enableCsrfCookie && !Yii::$app->request->getCookies()->count) {
+        if ($this->getExceptionCode() == $badRequestExceptionCode) {
+            if ($this->controller->enableCsrfValidation && Yii::$app->request->enableCsrfCookie && !Yii::$app->request->getCookies()->count) {
                 $cookiesDisabled = true;
             }
         }
