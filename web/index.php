3f6c12dd9d0e137e7b517a43f6ad987ec4cdad43
Vitaliy Shevchenko <pro2847@gmail.com>
2020-05-08 17:13:41 +0700

    requirements,php update and composer update

--- web/index.php
+++ web/index.php
@@ -16,6 +16,9 @@ require __DIR__ . '/../vendor/yiisoft/yii2/Yii.php';
 
 Yii::$classMap['yii\helpers\Html'] = '@app/components/Html.php';
 
-$config = require __DIR__ . '/../config/web.php';
+$config = yii\helpers\ArrayHelper::merge(
+    require(__DIR__ . '/../config/web.php'),
+    require(__DIR__ . '/../config/web-local.php')
+);
 
 (new yii\web\Application($config))->run();
