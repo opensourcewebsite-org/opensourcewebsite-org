845b88026638df2a4800537e70be2f776662f162
Lau-25 <lau_20@mail.ru>
2020-05-11 11:33:34 +0300

    Update no-requirements.php

--- modules/bot/views/privates/s-ce/no-requirements.php
+++ modules/bot/views/privates/s-ce/no-requirements.php
@@ -2,15 +2,17 @@
 <br/>
 <?= Yii::t('bot', 'To start using this service follow these steps') ?>:<br/>
 
-<?php if (!$telegramUser->location_lon && !$telegramUser->location_lat) {
-	    echo (Yii::t('bot', 'Send your location'));
-    }
+<?php
+if (!$telegramUser->location_lon && !$telegramUser->location_lat) {
+    echo (Yii::t('bot', 'Send your location'));
+}
 ?>
 <br/>
 /my_location - <?= Yii::t('bot', 'Location') ?><br/>
 <br/>
-<?php if (!$telegramUser->provider_user_name) {
-        echo (Yii::t('bot', 'Set your nickname in your Telegram account'));
-    }
+<?php
+if (!$telegramUser->provider_user_name) {
+    echo (Yii::t('bot', 'Set your nickname in your Telegram account'));
+}
 ?>
 <br/>/my_profile - <?= Yii::t('bot', 'Profile') ?><br/>
