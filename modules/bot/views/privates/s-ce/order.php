bd6c776abd789047c4d27e1a7fe4cc154d7627bb
Lau-25 <lau_20@mail.ru>
2020-05-12 09:10:45 +0300

    Update order.php

--- modules/bot/views/privates/s-ce/order.php
+++ modules/bot/views/privates/s-ce/order.php
@@ -1,10 +1,10 @@
-<b><?= $selling_currency . '/' . $buying_currency?> order #1020304050</b><br/>
+<b><?php echo($selling_currency . '/' . $buying_currency);?> order #1020304050</b><br/>
 <b><?= ($optional_name !== '' ? Yii::t('bot', 'Optional name') . ': ' . $optional_name : '');?></b><br/>
 <br/>
 <?= Yii::t('bot', 'Sell') ?>: <b><?= $selling_currency ?></b><br/>
 <?= Yii::t('bot', 'Amount') ?>: <?= $selling_currency_min_amount . ' - ' . $selling_currency_max_amount ?><br/>
 <?= Yii::t('bot', 'Payment methods') ?>:<br/>
-<?php  
+<?=
 	foreach ($sellingPaymentMethod as $value) {
 		if ($value['name'] == 'Cash') {
 			echo ' - ' . $value['name'] . '<br/>';
@@ -19,7 +19,7 @@
 <br/>
 <?= Yii::t('bot', 'Buy')?>: <b><?= $buying_currency?></b><br/>
 <?= Yii::t('bot', 'Payment methods') ?>:<br/>
-<?php  
+<?=
 	foreach ($buyingPaymentMethod as $value) {
 		if ($value['name'] == 'Cash') {
 			echo ' - ' . $value['name'] . '<br/>';
