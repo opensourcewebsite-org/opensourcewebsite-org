c3f970c9e8d52f609ec2e4b33f22ff32e7e1bf5b
Serj <kraspvp@mail.ru>
2020-05-12 16:31:33 +0700

    fix style

--- views/support-groups/_form.php
+++ views/support-groups/_form.php
@@ -41,15 +41,17 @@ use yii\widgets\ActiveForm;
             </div>
 
             <div class="card-footer">
-                <?= SaveButton::widget(); ?>
-                <?= CancelButton::widget([
-                    'url' => '/support-groups'
-                ]); ?>
-                <?php if (!$model->isNewRecord) {
-                    echo DeleteButton::widget([
-                        'url' => ['delete', 'id' => $model->id]
+                <?php
+                    echo SaveButton::widget();
+                    echo CancelButton::widget([
+                        'url' => '/support-groups'
                     ]);
-                } ?>
+                    if (!$model->isNewRecord) {
+                        echo DeleteButton::widget([
+                            'url' => ['delete', 'id' => $model->id]
+                        ]);
+                    }
+                ?>
             </div>
             <?php ActiveForm::end(); ?>
         </div>
