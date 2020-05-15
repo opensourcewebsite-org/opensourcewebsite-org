e2c0bf52ff64c588cf2173eef09152e6283c5fb7
GrytsenkoAndrey <andreyapg@gmail.com>
2020-05-15 14:22:55 +0300

    fix #359 Messages translation
    Description of the Change
    Added new 'common.php' file to the '/messages/ru/'
    Replaced 'bot' and 'app' usages of duplicate keys from the both files (arrays)
    
    Alternate Designs
    
    Benefits
    From this moment it's good to use 'common.php' array for global text
    
    Possible Drawbacks
    
    Verification Process
    
    Applicable Issues

--- modules/bot/views/privates/my-referrals/invite-template.php
+++ modules/bot/views/privates/my-referrals/invite-template.php
@@ -1,6 +1,6 @@
 OpenSourceWebsite (OSW) - <?= Yii::t('common', 'online community managed by users using electronic voting and modifying source code'); ?>.<br/>
 <br/>
-<?= Yii::t('bot', 'Join us and be part of the digital future'); ?>!<br/>
+<?= Yii::t('common', 'Join us and be part of the digital future'); ?>!<br/>
 <br/>
 <a href="<?= $websiteRefUrl ?>"><?= Yii::t('bot', 'Website'); ?></a>
 <br/>
