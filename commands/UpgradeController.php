4e88ae0e07567f29b50e1df2d9a8ae4489de86cd
GrytsenkoAndrey <andreyapg@gmail.com>
2020-05-20 10:55:45 +0300

    fix #424 Create command/UpgradeController.php
    Description of the Change
    Added /commands/UpgradeController to change DataBase' and Tables' charset in utf8mb4_0900_ai_ci
    Removed two migrations
    - m190624_144712_update_database_charset.php
    - m190627_143347_update_table_charset.php
    
    Alternate Designs
    
    Benefits
    
    Possible Drawbacks
    
    Verification Process
    
    Applicable Issues

---
+++ commands/UpgradeController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace app\commands;
+
+use yii\db\Migration;
+
+class UpgradeController extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        preg_match('/' . 'dbname' . '=([^;]*)/', $this->db->dsn, $match);
+        $dbName = $match[1];
+        if ($this->execute("ALTER DATABASE $dbName CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci;")) {
+            $tableSchemas = $this->db->schema->getTableSchemas();
+            $this->execute("SET foreign_key_checks = 0;");
+            foreach ($tableSchemas as $tableSchema) {
+                $this->execute("ALTER TABLE $tableSchema->name CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci;");
+            }
+            $this->execute("SET foreign_key_checks = 0;");
+        }
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        echo "UpgradeController cannot be reverted.\n";
+
+        return false;
+    }
+}
