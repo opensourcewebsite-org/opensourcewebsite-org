fd53ee212204c28b09a45f3b7ba1004197b699dc
Andrey Guzich <a.guzich@gmail.com>
2020-05-21 05:04:27 +0300

    add method isPublic to Types/Chat (#437)
    * first test commit
    
    * create voteban controller
    
    * Add feature voteban for groups (issue #306)
    
    * fix code style
    
    * fix code styles
    
    * fix code styles and other
    
    * fix code styles more
    
    * --
    
    * --
    
    * --
    
    * --
    
    * --
    
    * --
    
    * --
    
    * --
    
    * --
    
    * --
    
    * --
    
    * --
    
    * --
    
    * --
    
    * --
    
    * --
    
    * --
    
    * --
    
    * --
    
    * Update modules/bot/controllers/privates/AdminVoteBanController.php
    
    Co-Authored-By: Vladimir Strizhakov <vova_strigh@list.ru>
    
    * Update modules/bot/controllers/privates/AdminVoteBanController.php
    
    Co-Authored-By: Vladimir Strizhakov <vova_strigh@list.ru>
    
    * Update modules/bot/components/response/ResponseBuilder.php
    
    Co-Authored-By: Vladimir Strizhakov <vova_strigh@list.ru>
    
    * Update modules/bot/controllers/privates/AdminVoteBanController.php
    
    Co-Authored-By: Vladimir Strizhakov <vova_strigh@list.ru>
    
    * --
    
    * --
    
    * Telegram Bot. Groups. Refresh. #339
    https://github.com/opensourcewebsite-org/opensourcewebsite-org/issues/339
    
    * --
    
    * --
    
    * --
    
    * --
    
    * --
    
    * --
    
    * --
    
    * --
    
    * --
    
    * --
    
    * --
    
    * --
    
    * merge
    
    * Fix user rating, which was calculated from all chats
    
    * add method  getResponseBuilder to controller
    add method getMessage to controller (get messsage or edited message or
    messasge form callbackQuery)
    add group functionality for edited messages
    
    * replace getting ResponseBuilder in private controllers
    
    * --
    
    * исправление ошибки получения ResponseBuilder
    
    * лишний файл попал
    
    * убрал лишние пустые строки
    
    * --
    
    * замечания codeclimate
    
    * --
    
    * метод isPublic класса Types/Chat
    
    * --
    
    * --
    
    * --
    
    Co-authored-by: root <root@dedTdSpb.shneider-host.ru>
    Co-authored-by: Vladimir Strizhakov <vova_strigh@list.ru>
    Co-authored-by: Andrey Guzich <aguzich@gmail.com>

---
+++ modules/bot/components/api/Types/Message.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace app\modules\bot\components\api\Types;
+
+use TelegramBot\Api\Types\Payments\Invoice;
+use TelegramBot\Api\Types\Payments\SuccessfulPayment;
+use TelegramBot\Api\Types\User;
+use TelegramBot\Api\Types\ArrayOfMessageEntity;
+use TelegramBot\Api\Types\Audio;
+use TelegramBot\Api\Types\Document;
+use TelegramBot\Api\Types\ArrayOfPhotoSize;
+use TelegramBot\Api\Types\Sticker;
+use TelegramBot\Api\Types\Video;
+use TelegramBot\Api\Types\Voice;
+use TelegramBot\Api\Types\Contact;
+use TelegramBot\Api\Types\Location;
+use TelegramBot\Api\Types\Venue;
+
+class Message extends \TelegramBot\Api\Types\Message
+{
+    /**
+     * {@inheritdoc}
+     *
+     * @var array
+     */
+    static protected $map = [
+        'message_id' => true,
+        'from' => User::class,
+        'date' => true,
+        'chat' => Chat::class,
+        'forward_from' => User::class,
+        'forward_date' => true,
+        'reply_to_message' => Message::class,
+        'text' => true,
+        'entities' => ArrayOfMessageEntity::class,
+        'caption_entities' => ArrayOfMessageEntity::class,
+        'audio' => Audio::class,
+        'document' => Document::class,
+        'photo' => ArrayOfPhotoSize::class,
+        'sticker' => Sticker::class,
+        'video' => Video::class,
+        'voice' => Voice::class,
+        'caption' => true,
+        'contact' => Contact::class,
+        'location' => Location::class,
+        'venue' => Venue::class,
+        'new_chat_member' => User::class,
+        'left_chat_member' => User::class,
+        'new_chat_title' => true,
+        'new_chat_photo' => ArrayOfPhotoSize::class,
+        'delete_chat_photo' => true,
+        'group_chat_created' => true,
+        'supergroup_chat_created' => true,
+        'channel_chat_created' => true,
+        'migrate_to_chat_id' => true,
+        'migrate_from_chat_id' => true,
+        'pinned_message' => Message::class,
+        'invoice' => Invoice::class,
+        'successful_payment' => SuccessfulPayment::class,
+        'forward_signature' => true,
+        'author_signature' => true,
+        'connected_website' => true
+    ];
+}
