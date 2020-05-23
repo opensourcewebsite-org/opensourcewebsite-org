0647af6bb74143a4340375dc91b16139a1455455
Andrey Guzich <aguzich@gmail.com>
2020-05-23 22:21:37 +0300

    --

--- modules/bot/components/request/AliasCommandResolver.php
+++ modules/bot/components/request/AliasCommandResolver.php
@@ -4,6 +4,7 @@ namespace app\modules\bot\components\request;
 
 use TelegramBot\Api\Types\Update;
 use app\modules\bot\models\BotRouteAlias;
+use app\modules\bot\controllers\publics\VotebanController;
 
 class AliasCommandResolver implements ICommandResolver
 {
@@ -17,12 +18,31 @@ class AliasCommandResolver implements ICommandResolver
             $replyMessage = $message->getReplyToMessage();
             if ($replyMessage) {
                 $commandText = $message->getText();
-                $commandAlias = BotRouteAlias::find()->where(['text' => $commandText])->one();
-                if ($commandAlias) {
-                    $route = $commandAlias->command;
-                }
+                $route = $this->getStaticAlias($commandText);
+                $route = $route ?? $this->getGroupAlias($commandText);
             }
         }
+        return $route ?? null;
+    }
+
+    private function getStaticAlias($text)
+    {
+        $aliases = [
+            'voteban' => VotebanController::createRoute('index')
+        ];
+
+        return $aliases[$text] ?? null;
+    }
+
+    private function getGroupAlias($text)
+    {
+        if ($text) {
+            $routeAlias = BotRouteAlias::find()->where(['text' => $text])->one();
+        }
+        if ($routeAlias) {
+            $route = $routeAlias->route;
+        }
+
         return $route ?? null;
     }
 }
