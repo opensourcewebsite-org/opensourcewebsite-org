aa706f87e335b951a49097982f13554bd6b7c3a8
egorrishe <egorrishe@yandex.ru>
2020-05-23 17:25:53 +0300

    #294 ref: CodeClimate

--- components/debt/OutdatedObjectException.php
+++ components/debt/OutdatedObjectException.php
@@ -4,7 +4,7 @@ namespace app\components\debt;
 
 use yii\base\Exception;
 
-class OutdatedObjectException  extends Exception
+class OutdatedObjectException extends Exception
 {
     /**
      * @return string the user-friendly name of this exception
