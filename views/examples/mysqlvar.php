3676c9933cfb6417901296b19f6fe7a476d67acc
develr57 <47880662+develr57@users.noreply.github.com>
2020-05-29 19:19:10 +0500

    Update mysqlvar.php
    fixed table styles. Tested on browsers: SlimBrowser 12.0.10.0 (based on Firefox 72.0) and Microsoft Edge 83.0.478.37

--- views/examples/mysqlvar.php
+++ views/examples/mysqlvar.php
@@ -9,38 +9,32 @@ $this->params['breadcrumbs'][] = $this->title;
 $JS = <<<JS
     "use strict"
     $(document).ready(function(){
-        $(".table th, .table td").css({'word-break': 'break-all'});
+        $(".table td:last-child").css({'word-break': 'break-all'});
     })
 JS;
 $this->registerJs($JS);
 ?>
 
-<div class="issue-index">
-    <div class="">
-        <div class="col-12">
-            <div class="card">
-                <div class="card-body p-0">
-                    <div class="table-responsive">
-                        <div id="w0" class="grid-view">
-                            <table class="table table-condensed table-striped">
-                                <thead>
-                                    <tr>
-                                        <th class="col-5">Variable name</th>
-                                        <th class="col-7">Value</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    <?php foreach ($mysqlvars as $mysqlVar => $mysqlValue) : ?>
-                                        <tr>
-                                            <td class="col-5"><?php echo $mysqlVar; ?></td>
-                                            <td class="col-7"><?php echo $mysqlValue; ?></td>
-                                        </tr>
-                                    <?php endforeach; ?>
-                                </tbody>
-                            </table>
-                        </div>
-                    </div>
-                </div>
+<div class="card">
+    <div class="card-body p-0">
+        <div class="table-responsive">
+            <div id="w0" class="grid-view">
+                <table class="table table-condensed table-striped">
+                    <thead>
+                        <tr>
+                            <th scope="col">Variable name</th>
+                            <th scope="col">Value</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php foreach ($mysqlvars as $mysqlVar => $mysqlValue) : ?>
+                            <tr>
+                                <td><?php echo $mysqlVar; ?></td>
+                                <td><?php echo $mysqlValue; ?></td>
+                            </tr>
+                        <?php endforeach; ?>
+                    </tbody>
+                </table>
             </div>
         </div>
     </div>
