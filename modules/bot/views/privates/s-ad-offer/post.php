64f9e5656f296dd78a8ab6d7ed5e2b673e2b11a9
Vadim Berinchik <berinchik2012@gmail.com>
2020-06-09 09:23:28 -0500

    Telegram Bot. Ads (#461)
    * Ads
    
    * Translated added
    
    * Location text added and keywords not starting with #
    
    * Slack comments added
    
    * Codeclimate fixed
    
    * Some fixes add
    
    * Cron added
    
    * Fixes add
    
    * Codeclimate fix
    
    * Models change
    
    * Commets fix
    
    * Fixes + SQL search
    
    * Fixes
    
    * Fixes
    
    * Check ad status
    
    * Fix offers
    
    * Search keywords
    
    * 2 factor search
    
    * Fix migrations
    
    * Simplify search algorithm
    
    * Fix typo
    
    * Fix search
    
    * Fix mysql
    
    * Fix codeclimate trailing spaces
    
    * Fix icons
    
    * Fix in-development str
    
    * Fix icons 2
    
    * Fix no requirements

---
+++ modules/bot/views/privates/s-ad-offer/post.php
@@ -0,0 +1,21 @@
+💰 <b><?= $sectionName ?></b><br/>
+<br/>
+<?= $adOffer->title ?><br/>
+<br/>
+<?php if ($keywords != '') : ?>
+<b><?= Yii::t('bot', 'Keywords') ?>:</b> <?= $keywords ?><br/>
+<?php endif; ?>
+<?php if ($adOffer->description !== null) : ?>
+<b><?= Yii::t('bot', 'Description') ?>:</b> <?= $adOffer->description ?><br/>
+<?php endif; ?>
+<?php if (isset($adOffer->price) && isset($currency)) : ?>
+<b><?= Yii::t('bot', 'Price') ?>:</b> <?= $adOffer->price ?> <?= $currency->code ?><br/>
+<?php endif; ?>
+<b><?= Yii::t('bot', 'Location') ?>:</b> <a href = "<?= $locationLink ?>"><?= $adOffer->location_lat ?> <?= $adOffer->location_lon ?></a><br/>
+<?php if ($adOffer->delivery_radius > 0) : ?>
+<b><?= Yii::t('bot', 'Delivery radius') ?>:</b> <?= $adOffer->delivery_radius ?> <?= Yii::t('bot', 'km') ?><br/>
+<?php endif; ?>
+<br/>
+<?php if ($adOffer->isActive() && $showDetailedInfo) : ?>
+<i><?= Yii::t('bot', 'This ad will be active for') ?> <?= $liveDays ?> <?= Yii::t('bot', 'days. Visit this page again before expiring time to automatically extend ad action') ?>.</i>
+<?php endif; ?>
