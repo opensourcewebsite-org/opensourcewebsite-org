9b6c40eafb52b5ac11daa25f956ed5383f6e3a36
dnlsu <67639883+dnlsu@users.noreply.github.com>
2020-07-05 18:07:41 +0700

    Milestone2p3 (#497)
    * milestone 2. part 3
    * rate and languages coincidence
    
    * milestone 2. part 3
    * JobMatchController.php created
    
    * milestone 2. part 3
    * Vacancy.php user_id filling every time
    * contact was added to matches
    
    * milestone 2. part 3
    * keywords connected to vacancy
    * keywords connected to resume
    
    * milestone 2. part 3
    * show keywords
    * createIfEmpty parameter was added to CrudController rule attributes
    
    * milestone 2. part 3
    * refactoring
    
    * milestone 2. part 3
    * code fixed
    
    Co-authored-by: Dnl Su <git.vcs@dnl.su>

---
+++ modules/bot/models/JobKeyword.php
@@ -0,0 +1,61 @@
+<?php
+
+
+namespace app\modules\bot\models;
+
+use app\models\Resume;
+use app\models\Vacancy;
+use yii\db\ActiveRecord;
+
+/**
+ * Class JobKeyword
+ *
+ * @package app\modules\bot\models
+ */
+class JobKeyword extends ActiveRecord
+{
+    /** @inheritDoc */
+    public static function tableName()
+    {
+        return '{{%job_keyword}}';
+    }
+
+    /** @inheritDoc */
+    public function rules()
+    {
+        return [
+            [['keyword'], 'required'],
+            [['keyword'], 'string'],
+            [['keyword'], 'unique'],
+        ];
+    }
+
+    public function getVacanciesRelation()
+    {
+        return $this->hasMany(Vacancy::className(), ['id' => 'vacancy_id'])
+            ->viaTable('{{%job_vacancy_keyword}}', ['job_keyword_id' => 'id']);
+    }
+
+    /**
+     * @return string
+     */
+    public function getJobKeywordLabel()
+    {
+        return $this->keyword;
+    }
+
+    public function getResumesRelation()
+    {
+        return $this->hasMany(Resume::className(), ['id' => 'resume_id'])
+            ->viaTable('{{%job_resume_keyword}}', ['job_keyword_id' => 'id']);
+    }
+
+    /** @inheritDoc */
+    public static function find()
+    {
+        $query = parent::find();
+        $query->orderBy(['keyword' => SORT_ASC]);
+
+        return $query;
+    }
+}
---
+++ modules/bot/models/JobVacancyKeyword.php
@@ -0,0 +1,29 @@
+<?php
+
+
+namespace app\modules\bot\models;
+
+use yii\db\ActiveRecord;
+
+/**
+ * Class JobVacancyKeyword
+ *
+ * @package app\modules\bot\models
+ */
+class JobVacancyKeyword extends ActiveRecord
+{
+    /** @inheritDoc */
+    public static function tableName()
+    {
+        return '{{%job_vacancy_keyword}}';
+    }
+
+    /** @inheritDoc */
+    public function rules()
+    {
+        return [
+            [['vacancy_id', 'job_keyword_id'], 'required'],
+            [['vacancy_id', 'job_keyword_id'], 'integer'],
+        ];
+    }
+}
