6dab33e06467cd94088906b3cf0a78095f7863ef
Alexeis <tanks.eu@gmail.com>
2020-07-13 23:50:46 +0300

    #473 exclude chat admins from captcha

--- modules/bot/components/request/SystemMessageCommandResolver.php
+++ modules/bot/components/request/SystemMessageCommandResolver.php
@@ -4,13 +4,34 @@ namespace app\modules\bot\components\request;
 use app\modules\bot\controllers\publics\JoinCaptchaController;
 use app\modules\bot\controllers\publics\SystemMessageController;
 use TelegramBot\Api\Types\Update;
+use app\modules\bot\models\ChatMember;
 
 class SystemMessageCommandResolver implements ICommandResolver
 {
     public function resolveCommand(Update $update)
     {
+
         if ($update->getMessage() && $update->getMessage()->getNewChatMember()) {
-            $commandText = JoinCaptchaController::createRoute('show-captcha');
+
+            $chatId = $update->getMessage()->getChat()->getId();
+            $telegramUser = $update->getMessage()->getFrom();
+            $isAdmin = false;
+            if (isset($chat) && isset($user)){
+                $chatMember = ChatMember::find()
+                            ->select('bot_chat_member.id')
+                            ->leftJoin('bot_chat','bot_chat_member.chat_id = bot_chat.id')
+                            ->leftJoin('bot_user','bot_chat_member.user_id = bot_user.id')
+                            ->where(['bot_chat.chat_id' => $chatId, 'bot_user.provider_user_id' => $telegramUser->getId()])->one();
+
+            }
+            if(isset($chatMember)){
+                $isAdmin = $chatMember->isAdmin();
+            }
+            if(!$isAdmin) {
+                $commandText = JoinCaptchaController::createRoute('show-captcha');
+            }
+
+
         }
 
         if ($update->getMessage() && $update->getMessage()->getLeftChatMember()) {
