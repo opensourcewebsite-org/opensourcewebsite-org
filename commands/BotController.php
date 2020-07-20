590b9670ef241500822ebbbf0d70b6c4b7f0cb6b
Alexeis <tanks.eu@gmail.com>
2020-07-21 00:01:22 +0300

    #473 Added kicking user from chat after time interval. Usage  yii bot/ban-unverified-users. Reverted back link assets

--- commands/BotController.php
+++ commands/BotController.php
@@ -2,11 +2,14 @@
 
 namespace app\commands;
 
+use app\modules\bot\components\helpers\MessageText;
+use app\modules\bot\models\BotChatCaptcha;
 use Yii;
 use yii\console\Controller;
 use app\interfaces\CronChainedInterface;
 use app\commands\traits\ControllerLogTrait;
 use app\modules\bot\models\Bot;
+use app\commands\CronController;
 
 /**
  * Class BotController
@@ -103,4 +106,37 @@ class BotController extends Controller implements CronChainedInterface
         echo 'Bot with the same token already exists';
         return false;
     }
+
+    public function actionBanUnverifiedUsers()
+    {
+
+        $bots = Bot::findAll(['status' => Bot::BOT_STATUS_ENABLED]);
+        if(isset($bots)) {
+
+            foreach ($bots as $bot) {
+
+                $botApi = new \TelegramBot\Api\BotApi($bot->token);
+
+                $usersToBan = BotChatCaptcha::find()
+                    ->with('chat')
+                    ->leftJoin('bot_chat', 'bot_chat_captcha.chat_id = bot_chat.id')
+                    ->leftJoin('bot', 'bot_chat.bot_id = bot.id')
+                    ->where(['<', 'sent_at', time() - CronController::INTERVAL])
+                    ->andFilterWhere(['bot.id' => $bot->id])->all();
+
+                if (isset($usersToBan)) {
+                    foreach ($usersToBan as $record) {
+                        BotChatCaptcha::deleteAll([
+                            'chat_id' => $record->chat_id,
+                            'provider_user_id' =>  $record->provider_user_id
+                        ]);
+                        $botApi->kickChatMember($record->chat->chat_id, $record->provider_user_id);
+                    }
+                }
+
+            }
+        }
+
+    }
+
 }
