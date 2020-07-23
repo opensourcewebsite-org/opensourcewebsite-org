8ca5580e628a2ef0089f2f24db4414e20ea98ca5
Vitaliy Shevchenko <grandmotivator@gmail.com>
2020-07-23 20:01:20 +0700

    bot clean code

--- modules/bot/controllers/privates/MyAccountController.php
+++ modules/bot/controllers/privates/MyAccountController.php
@@ -22,35 +22,9 @@ class MyAccountController extends Controller
      */
     public function actionIndex()
     {
-        $telegramUser = $this->getTelegramUser();
-        $user = $this->getUser();
-        $timezones = TimeHelper::timezonesList();
-
-        $currency = $user->currency;
-
-        $params = [
-            'firstName' => $telegramUser->provider_user_first_name,
-            'lastName' => $telegramUser->provider_user_last_name,
-            'username' => $telegramUser->provider_user_name,
-            'gender' => isset($user->gender) ? $user->gender->name : null,
-            'sexuality' => isset($user->sexuality) ? $user->sexuality->name : null,
-            'birthday' => $user->birthday,
-            'currency' => isset($currency) ? "{$currency->name} ({$currency->code})" : null,
-            'timezone' => $timezones[$user->timezone],
-            'languages' => array_map(function ($userLanguage) {
-                return $userLanguage->getDisplayName();
-            }, $user->languages),
-            'citizenships' => array_map(function ($citizenship) {
-                return $citizenship->country->name;
-            }, $user->citizenships),
-            'location_lat' => $telegramUser->location_lat,
-            'location_lon' => $telegramUser->location_lon,
-            'locationLink' => ExternalLink::getOSMLink($telegramUser->location_lat, $telegramUser->location_lon),
-        ];
-
         return $this->getResponseBuilder()
             ->editMessageTextOrSendMessage(
-                $this->render('index', $params),
+                $this->render('index'),
                 [
                     [
                         [
