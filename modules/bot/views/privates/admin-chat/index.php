b4a3b9145fc60e8f776e45b4121aa2bc649c48e8
Vitaliy Shevchenko <grandmotivator@gmail.com>
2020-07-29 12:49:20 +0700

    bot group

--- modules/bot/views/privates/admin-chat/index.php
+++ modules/bot/views/privates/admin-chat/index.php
@@ -1,3 +1,15 @@
 <b><?= $chatTitle ?></b><br/>
 <br/>
-<?= Yii::t('bot', 'Select a feature to manage the group') ?>.
+<?= Yii::t('bot', 'Select a feature to manage the group') ?>.<br/>
+<br/>
+<?= Yii::t('bot', 'Administrators who can manage the group') ?>:<br/>
+<?php
+foreach ($admins as $user) {
+    if ($user->provider_user_name) {
+        $userLinks[] = '@' . $user->provider_user_name;
+    } else {
+        $userLinks[] = '<a href="tg://user?id=' . $user->provider_user_id .'">' . $user->provider_user_first_name . ' ' . $user->provider_user_last_name . '</a>';
+    }
+}
+?>
+<?= implode(', ', $userLinks); ?>
