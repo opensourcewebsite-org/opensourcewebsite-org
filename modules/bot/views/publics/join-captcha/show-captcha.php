abb2a6b9d88d15242e65afcc1d8e5333e7f9e328
Vitaliy Shevchenko <grandmotivator@gmail.com>
2020-07-30 13:37:37 +0700

    bot texts

--- modules/bot/views/publics/join-captcha/show-captcha.php
+++ modules/bot/views/publics/join-captcha/show-captcha.php
@@ -2,13 +2,7 @@
 /**
  * @var $user app\modules\bot\models\User
  */
-
-if ($user->provider_user_name) {
-    $userLink = '@' . $user->provider_user_name;
-} else {
-    $userLink = '<a href="tg://user?id=' . $user->provider_user_id .'">' . $user->provider_user_first_name . ' ' . $user->provider_user_last_name . '</a>';
-}
 ?>
-<?= Yii::t('bot', 'Welcome') ?>, <?= $userLink ?>!<br/>
+<?= Yii::t('bot', 'Welcome') ?>, <?= $user->getFullLink(); ?>!<br/>
 <br/>
 <?= Yii::t('bot', 'Before sending any messages, press {0} to verify that you are a human', '👍') ?>. <?= Yii::t('bot', 'If you don\'t solve the captcha in {0,number} mins, you will be automatically kicked out of the group', 5) ?>.<br/>
