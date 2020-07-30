b44b6f2606c98294c4d80eaf79055fa634dcf7b9
Vitaliy Shevchenko <grandmotivator@gmail.com>
2020-07-30 20:01:46 +0700

    debts start date

--- views/debt/index.php
+++ views/debt/index.php
@@ -16,7 +16,7 @@ $this->params['breadcrumbs'][] = $this->title;
 <div class="callout callout-danger">
     <h5><?= Yii::t('app', 'Attention') ?>!</h5>
     <p>
-        <?= Yii::t('app', 'This feature works in test mode') ?>. <?= Yii::t('app', 'Please help to test all functions of this') ?>. <?= Yii::t('app', 'All data of debts will be deleted from 2020-08-01 or earlier') ?>. <?= Yii::t('app', 'After that, this feature will work in an operating mode') ?>.
+        <?= Yii::t('app', 'This feature works in test mode') ?>. <?= Yii::t('app', 'Please help to test all functions of this') ?>. <?= Yii::t('app', 'All data of debts will be deleted from {0} or earlier', '2020-09-01') ?>. <?= Yii::t('app', 'After that, this feature will work in an operating mode') ?>.
     </p>
 </div>
 
@@ -29,11 +29,11 @@ $this->params['breadcrumbs'][] = $this->title;
                         <div class="col-sm-6">
                             <ul class="nav nav-pills">
                                 <li class="nav-item">
-                                    <?php 
+                                    <?php
                                         if ((int)Debt::countStatusPending() !== 0) {
                                             echo Html::a(Yii::t('app', 'New'), ['debt/index',], [
                                                 'class' => 'btn btn-outline-primary mr-2',
-                                            ]); 
+                                            ]);
                                         }
                                     ?>
                                 </li>
