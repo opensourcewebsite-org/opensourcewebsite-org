51b9ec56cb2d7645fd96fffb0eb30dff9c434a0f
Vitaliy Shevchenko <grandmotivator@gmail.com>
2020-08-07 19:15:35 +0700

    bot group voteban fix

--- modules/bot/views/publics/voteban/user-kicked.php
+++ modules/bot/views/publics/voteban/user-kicked.php
@@ -7,6 +7,6 @@ use app\modules\bot\components\helpers\Emoji;
 <?= Emoji::KICK_VOTE . ' ' . Yii::t('bot', '{user} has been kicked — the only way to get this user back is for admins to manualy unban in group settings', ['user' => User::getFullLinkByProviderUserId($providerCandidateId)]) ?>.<br/>
 <br/>
 <?= Yii::t('bot', 'Voters who chose to kick') ?>:<br/>
-<?php foreach ($voterIds as $providerVoterId) : ?>
+<?php foreach ($votersIds as $providerVoterId) : ?>
   <?= User::getFullLinkByProviderUserId($providerVoterId); ?><br/>
 <?php endforeach; ?>
--- modules/bot/views/publics/voteban/user-saved.php
+++ modules/bot/views/publics/voteban/user-saved.php
@@ -7,6 +7,6 @@ use app\modules\bot\components\helpers\Emoji;
 <?= Emoji::SAVE_VOTE . ' ' . Yii::t('bot', '{user} has been saved — no kick for you this time', ['user' => User::getFullLinkByProviderUserId($providerCandidateId)]) ?>.<br/>
 <br/>
 <?= Yii::t('bot', 'Voters who chose to save') ?>:<br/>
-<?php foreach ($voterIds as $providerVoterId) : ?>
+<?php foreach ($votersIds as $providerVoterId) : ?>
   <?= User::getFullLinkByProviderUserId($providerVoterId); ?><br/>
 <?php endforeach; ?>
