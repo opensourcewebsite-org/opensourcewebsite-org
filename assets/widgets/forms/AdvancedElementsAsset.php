3d27b3050a1ae4576d971a8ea877b2efa508a00e
viper_work <wiperawa@gmail.com>
2021-02-24 13:49:16 +0500

    fix assets for advanced-elements

--- assets/widgets/forms/AdvancedElementsAsset.php
+++ assets/widgets/forms/AdvancedElementsAsset.php
@@ -13,6 +13,8 @@ class AdvancedElementsAsset extends AssetBundle
         'https://cdn.jsdelivr.net/npm/select2@4.0.13/dist/css/select2.min.css',
         'https://cdn.jsdelivr.net/npm/select2-bootstrap4-theme@1.0.0/dist/select2-bootstrap4.min.css',
         'https://cdn.jsdelivr.net/npm/bootstrap4-duallistbox@4.0.2/dist/bootstrap-duallistbox.min.css',
+        'https://cdn.jsdelivr.net/npm/daterangepicker/daterangepicker.css',
+        'https://cdnjs.cloudflare.com/ajax/libs/jquery-datetimepicker/2.5.20/jquery.datetimepicker.min.css',
     ];
 
     public $js = [
@@ -21,12 +23,15 @@ class AdvancedElementsAsset extends AssetBundle
         'https://cdn.jsdelivr.net/npm/jquery.inputmask@3.3.4/dist/jquery.inputmask.bundle.js',
         'https://cdn.jsdelivr.net/npm/bootstrap-colorpicker@3.2.0/dist/js/bootstrap-colorpicker.min.js',
         'https://cdn.jsdelivr.net/npm/bootstrap-switch@3.4.0/dist/js/bootstrap-switch.min.js',
+        'https://cdn.jsdelivr.net/momentjs/latest/moment.min.js',
+        'https://cdn.jsdelivr.net/npm/daterangepicker/daterangepicker.min.js',
+        'https://cdnjs.cloudflare.com/ajax/libs/jquery-datetimepicker/2.5.20/jquery.datetimepicker.full.min.js',
     ];
 
     public $depends = [
         'yii\web\YiiAsset',
         'yii\bootstrap4\BootstrapAsset',
         'yii\bootstrap4\BootstrapPluginAsset',
-        'app\assets\widgets\CommonAsset',
+        //'app\assets\widgets\CommonAsset',
     ];
 }
