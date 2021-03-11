479554d566f50693b4b6f3a8e881eb38fa3b8a37
Vitaliy Shevchenko <grandmotivator@gmail.com>
2021-03-11 15:34:25 +0700

    composer update

--- components/AssetManager.php
+++ components/AssetManager.php
@@ -7,34 +7,5 @@ use yii\helpers\Url;
 
 class AssetManager extends \yii\web\AssetManager
 {
-    /**
-     * @inheritdoc
-     */
-    public function getAssetUrl($bundle, $asset)
-    {
-        if (($actualAsset = $this->resolveAsset($bundle, $asset)) !== false) {
-            if (strncmp($actualAsset, '@web/', 5) === 0) {
-                $asset = substr($actualAsset, 5);
-                $basePath = Yii::getAlias('@webroot');
-                $baseUrl = Yii::getAlias('@web');
-            } else {
-                $asset = Yii::getAlias($actualAsset);
-                $basePath = $this->basePath;
-                $baseUrl = $this->baseUrl;
-            }
-        } else {
-            $basePath = Yii::getAlias($bundle->basePath);
-            $baseUrl = Yii::getAlias($bundle->baseUrl);
-        }
 
-        if (!Url::isRelative($asset) || strncmp($asset, '/', 1) === 0) {
-            return $asset;
-        }
-
-        if ($this->appendTimestamp && ($timestamp = @filemtime("$basePath/$asset")) > 0) {
-            return "$baseUrl/$asset?v=$timestamp";
-        } else {
-            return "$baseUrl/$asset";
-        }
-    }
 }
