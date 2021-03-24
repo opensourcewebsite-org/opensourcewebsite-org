4c1378fcf5cc62c410b5af8b801ee9d937474b36
viper_work <wiperawa@gmail.com>
2021-03-24 13:52:04 +0500

    lat/lon validators refactor selling_location/buying_location validator. saving only correct locations.

--- modules/bot/validators/LocationLatValidator.php
+++ modules/bot/validators/LocationLatValidator.php
@@ -14,12 +14,17 @@ class LocationLatValidator extends Validator
     /** @inheritDoc */
     public function validateAttribute($model, $attribute)
     {
-        if (!(is_numeric($model->$attribute)
-            && (doubleval($model->$attribute) >= -90) && (doubleval($model->$attribute) <= 90))) {
+        if (!$this->validateLat($model->$attribute)){
             $this->addError($model, $attribute, 'Input value must be between {min} and {max}', [
                 'min' => -90,
                 'max' => 90,
             ]);
         }
+    }
+
+    public function validateLat($lat): bool
+    {
+        return  (is_numeric($lat)
+            && (doubleval($lat) >= -90) && (doubleval($lat) <= 90));
     }
 }
