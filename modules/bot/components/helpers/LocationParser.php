ebf247f104e894e9332f98c6dafe6d7510c5d52c
wiperawa <wiperawa@gmail.com>
2021-03-25 09:41:20 +0500

    LocationParser for lat/lon (#545)
    * 'my location' in location modal.
    allow empty selling/buying rates (show all matched orders, with our without rates specified.
    
    * bugfixes
    
    * bug, sometime selling rate round to int.
    
    * bug with saving new order with empty rate(s)
    
    * lat/lon validators refactor
    selling_location/buying_location validator. saving only correct locations.
    
    * parsing location with LocationToArrayFieldComponent
    
    * extract LocationParsers from LocationToArrayFieldComponent

---
+++ modules/bot/components/helpers/LocationParser.php
@@ -0,0 +1,53 @@
+<?php
+namespace app\modules\bot\components\helpers;
+
+class LocationParser
+{
+    private const MAIN_DELIMITER = '|';
+
+    private array $delimiters;
+
+    private string $locationStr;
+
+    public function __construct(string $locationStr, array $delimiters = [',', ' ', ';', "\n"])
+    {
+        $this->locationStr = $locationStr;
+        $this->delimiters = $delimiters;
+    }
+
+    /**
+     * @return int[]|null[]|string[]
+     */
+    public function parse(): array
+    {
+        $latitude = null;
+        $longitude = null;
+
+        $text = str_replace($this->delimiters, self::MAIN_DELIMITER, $this->locationStr);
+        $text = str_replace(self::MAIN_DELIMITER . self::MAIN_DELIMITER, self::MAIN_DELIMITER, $text);
+
+        $coords = explode(self::MAIN_DELIMITER, $text);
+
+        $coords = array_map('trim', $coords);
+        if (count($coords) === 2) {
+            $coords = array_map( function($item) {
+                return preg_replace("|[^0-9\.]|", "$2", $item);
+                },
+                $coords);
+
+            if (is_numeric($coords[0]) && is_numeric($coords[1])) {
+                $latitude = $coords[0];
+                $longitude = $coords[1];
+            }
+        }
+
+        return [$latitude, $longitude];
+    }
+
+    public function withLocationStr(string $str): self
+    {
+        $clone = clone $this;
+        $this->locationStr = $str;
+        return $clone;
+    }
+}
