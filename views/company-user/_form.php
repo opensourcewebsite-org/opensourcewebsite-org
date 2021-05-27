06ec2fd03a787d6418940637efccbcf6a65ee9ec
wiperawa <wiperawa@gmail.com>
2021-05-27 17:58:51 +0500

    company form, cancel button

--- views/company-user/_form.php
+++ views/company-user/_form.php
@@ -44,7 +44,7 @@ use app\widgets\buttons\CancelButton;
                 </div>
                 <div class="card-footer">
                     <?= SaveButton::widget(); ?>
-                    <?= CancelButton::widget(['url' => '/company']); ?>
+                    <?= CancelButton::widget(['url' => '/company-user']); ?>
                     <?php if (!$companyUserModel->isNewRecord): ?>
                         <?= DeleteButton::widget([
                             'url' => ['delete', 'id' => $companyUserModel->id],
