890141422b61b3145827508b43933a5c07a11dbe
Vitaliy Shevchenko <grandmotivator@gmail.com>
2021-05-28 11:02:52 +0700

    jobs fix

--- views/company-user/update.php
+++ views/company-user/update.php
@@ -11,7 +11,7 @@ use yii\web\View;
  * @var array $_params_
  */
 
-$this->title = Yii::t('app', 'Update Company') . ' #' . $companyModel->name;
+$this->title = Yii::t('app', 'Update Company') . ' #' . $companyUserModel->id;;
 $this->params['breadcrumbs'][] = ['label' => Yii::t('app', 'Companies'), 'url' => ['index']];
 $this->params['breadcrumbs'][] = ['label' => '#' . $companyUserModel->id, 'url' => ['view', 'id' => $companyUserModel->id]];
 $this->params['breadcrumbs'][] = Yii::t('app', 'Update');
