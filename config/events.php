3fb40b0d0ff3f3a2a2e20cd2cef28c82d20fea6e
wiperawa <wiperawa@gmail.com>
2021-06-03 20:45:14 +0500

    move beforeRequest event to separate events.php file

---
+++ config/events.php
@@ -0,0 +1,18 @@
+<?php
+declare(strict_types=1);
+
+use yii\web\Application;
+
+return [
+    'on beforeRequest' => function ($event) {
+        /** @var Application $app */
+        $app = $event->sender;
+
+        /** @var app\models\User|null $user */
+        $user = $app->user->getIdentity();
+
+        if (($user !== null) && !(Yii::$app->user->isGuest)) {
+            $user->updateLastActivity();
+        }
+    }
+];
