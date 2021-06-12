261c788e9aa1973ae588ce02e171abaf6f1de28c
Mariohuq <mariohuq@github.com>
2021-06-12 17:56:39 +1000

    #484 Add preview of the answer in Markdown

--- modules/bot/views/privates/group-faq/set-answer.php
+++ modules/bot/views/privates/group-faq/set-answer.php
@@ -1,8 +1,15 @@
 <?php
 
 /* @var $this yii\web\View */
+/* @var $answerMarkdown string */
 
 ?>
 <b><?= Yii::t('bot', 'Send a message for the answer') ?>:</b><br/>
+<br/>
+————<br/>
+<br/>
+<pre><?= $answerMarkdown ?></pre><br/>
+<br/>
+————<br/>
 <br/>
 <?= $this->render("../formatting-options") ?>
