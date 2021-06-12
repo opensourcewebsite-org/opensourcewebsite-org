a19220c7a5df7582bf69d087b980edd1a17e5112
Mariohuq <mariohuq@github.com>
2021-06-12 15:40:06 +1000

    #484 Remove "coming soon" and "don't use html tags"

--- modules/bot/views/privates/group-faq/set-answer.php
+++ modules/bot/views/privates/group-faq/set-answer.php
@@ -5,11 +5,10 @@ use app\modules\bot\components\helpers\ExternalLink;
 ?>
 <b><?= Yii::t('bot', 'Send a message for the answer') ?>:</b><br/>
 <br/>
-<?= Yii::t('bot', 'Formatting options') ?> (<?= Yii::t('bot', 'coming soon') ?>):<br/>
+<?= Yii::t('bot', 'Formatting options') ?>:<br/>
   <b><?= Yii::t('bot', 'bold') ?></b> => <code>**<?= Yii::t('bot', 'bold') ?>**</code><br/>
   <i><?= Yii::t('bot', 'italic') ?></i> => <code>__<?= Yii::t('bot', 'italic') ?>__</code><br/>
   <strike><?= Yii::t('bot', 'strike') ?></strike> => <code>~~<?= Yii::t('bot', 'strike') ?>~~</code><br/>
   <code><?= Yii::t('bot', 'code') ?></code> => <code>`<?= Yii::t('bot', 'code') ?>`</code><br/>
   <a href="<?= ExternalLink::getBotLink() ?>"><?= Yii::t('bot', 'link title') ?></a> => <code>[<?= Yii::t('bot', 'link title') ?>](<?= Yii::t('bot', 'link') ?>)</code><br/>
 <br/>
-<i><?= Yii::t('bot', 'Don\'t use html tags') ?>.</i><br/>
