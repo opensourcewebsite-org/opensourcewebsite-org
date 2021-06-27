64c17dbf321e5f00a8adda50ba436e2cd7735ad5
mariohuq <mariohuq@github.com>
2021-06-28 00:05:03 +1000

    Add StellarServer::getDistributorPrivateKey, StellarServer::getAssetHolders, StellarServer::sendIncomeToAssetHolders

--- models/StellarServer.php
+++ models/StellarServer.php
@@ -4,12 +4,19 @@ namespace app\models;
 
 use Yii;
 use DateTime;
+use ZuluCrypto\StellarSdk\Horizon\Exception\PostTransactionException;
 use ZuluCrypto\StellarSdk\Model\Payment;
 use ZuluCrypto\StellarSdk\Server;
 use ZuluCrypto\StellarSdk\Horizon\ApiClient;
+use ZuluCrypto\StellarSdk\Util\MathSafety;
+use ZuluCrypto\StellarSdk\XdrModel\Asset;
+use ZuluCrypto\StellarSdk\XdrModel\Operation\PaymentOp;
 
 class StellarServer extends Server
 {
+    const INTEREST_RATE_WEEKLY = 0.5 / 100;
+    const MEMO_TEXT = 'Thanks For Your Deposit';
+
     public function __construct()
     {
         if (!isset(Yii::$app->params['stellar'])) {
@@ -56,8 +63,97 @@ class StellarServer extends Server
         return Yii::$app->params['stellar']['distributor_public_key'] ?? null;
     }
 
+    public function getDistributorPrivateKey(): ?string
+    {
+        return Yii::$app->params['stellar']['distributor_private_key'] ?? null;
+    }
+
     public function getOperatorPublicKey(): ?string
     {
         return Yii::$app->params['stellar']['operator_public_key'] ?? null;
+    }
+
+    /**
+     * @param string $assetCode
+     * @param float $minimumBalance
+     * @return \ZuluCrypto\StellarSdk\Model\Account[]
+     * @throws \ErrorException
+     * @throws \ZuluCrypto\StellarSdk\Horizon\Exception\HorizonException
+     */
+    public function getAssetHolders(string $assetCode, float $minimumBalance): array
+    {
+        MathSafety::require64Bit();
+
+        $assetIssuerId = $this->getIssuerPublicKey();
+        $blacklist = [$this->getDistributorPublicKey(), $this->getOperatorPublicKey()];
+
+        return array_filter(
+            $this->getAccountsForAsset($assetCode, $assetIssuerId, 'asc', 200),
+            fn ($a) => !in_array($a->getAccountId(), $blacklist)
+                && !empty(array_filter(
+                    $a->getBalances(),
+                    fn ($b) => $b->getAssetCode() === $assetCode
+                        && $b->getAssetIssuerAccountId() === $assetIssuerId
+                        && $b->getBalance() >= $minimumBalance
+                ))
+        );
+    }
+
+    /**
+     * @param string $assetCode
+     * @param \ZuluCrypto\StellarSdk\Model\Account[] $destinations
+     * @return array Schema: [['transaction' => TransactionBuilder, 'result' => TransactionResult]]
+     * Each element contains failed transaction and result, containing code of transaction result and codes
+     * for each operation in transaction.
+     * @throws \ErrorException
+     */
+    public function sendIncomeToAssetHolders(string $assetCode, array $destinations): array
+    {
+        MathSafety::require64Bit();
+
+        $interestRateWeekly = self::INTEREST_RATE_WEEKLY;
+        $memoText = self::MEMO_TEXT;
+
+        $income = function ($balance) use ($interestRateWeekly) {
+            $balance = floor($balance * 100.0) / 100.0;
+            return $balance * $interestRateWeekly;
+        };
+
+        $TRANSACTION_LIMIT = 100;
+
+        $assetIssuerId = $this->getIssuerPublicKey();
+        $publicKey = $this->getDistributorPublicKey();
+        $privateKey = $this->getDistributorPrivateKey();
+        $asset = Asset::newCustomAsset($assetCode, $assetIssuerId);
+
+        $payments = array_map(
+            fn ($d) => PaymentOp::newCustomPayment(
+                $publicKey,
+                $income($d->getCustomAssetBalanceValue($asset)),
+                $assetCode,
+                $assetIssuerId,
+                $publicKey
+            ),
+            $destinations
+        );
+
+        $results = [];
+
+        foreach (array_chunk($payments, $TRANSACTION_LIMIT) as $paymentGroup) {
+            $transaction = $this->buildTransaction($publicKey);
+            foreach ($paymentGroup as $payment) {
+                $transaction = $transaction->addOperation($payment);
+            }
+
+            try {
+                $transaction = $transaction
+                    ->setTextMemo($memoText);
+                $transaction->submit($privateKey);
+            } catch (PostTransactionException $e) {
+                $results[] = ['transaction' => $transaction, 'result' => $e->getResult()];
+            }
+        }
+
+        return $results;
     }
 }
