9ba48d3ed2eab9cb673a34660794261643cdec42
Vitaliy Shevchenko <grandmotivator@gmail.com>
2021-09-06 14:19:40 +0700

    bot fix routes

--- modules/bot/config/groups.php
+++ modules/bot/config/groups.php
@@ -8,6 +8,9 @@ $config = [
             'class' => GroupRouteResolver::class,
             'rules' => [
                 '/help' => 'hello/index',
+                '/my_face(@<botname:[\w_]+bot>)?' => 'my-fake-face/index',
+                '/my_cat(@<botname:[\w_]+bot>)?' => 'my-fake-cat/index',
+                '/my_art(@<botname:[\w_]+bot>)?' => 'my-fake-art/index',
                 '/<controller:\w+>__<action:\w+>(@<botname:[\w_]+bot>)?(\?<query:(&?\w+=[^&]*)*>)?( <message:.+>)?' => '<controller>/<action>',
                 '/<controller:\w+>(@<botname:[\w_]+bot>)?(\?<query:(&?\w+=[^&]*)*>)?( <message:.+>)?' => '<controller>/index',
             ],
