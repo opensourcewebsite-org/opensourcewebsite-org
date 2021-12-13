d2e3b1ef43728115689d32334aa7136e259c7c94
tim-btr <43270754+tim-btr@users.noreply.github.com>
2021-12-13 09:05:00 +0500

    Для всех страниц(кроме UI-general) пофиксены ошибки консоли (#598)

--- assets/widgets/dashboard/VisitorsAsset.php
+++ assets/widgets/dashboard/VisitorsAsset.php
@@ -15,7 +15,7 @@ class VisitorsAsset extends AssetBundle
 
     public $js = [
         'https://cdn.jsdelivr.net/npm/chart.js@2.9.3/dist/Chart.min.js',
-        'https://cdn.jsdelivr.net/npm/jquery-sparkline@2.4.0/jquery.sparkline.min.js',
+        //'https://cdn.jsdelivr.net/npm/jquery-sparkline@2.4.0/jquery.sparkline.min.js',
         'https://cdn.jsdelivr.net/npm/jqvmap@1.5.1/dist/jquery.vmap.min.js',
         'https://cdn.jsdelivr.net/npm/jqvmap@1.5.1/dist/maps/jquery.vmap.usa.js',
         'https://cdn.jsdelivr.net/npm/jquery-knob@1.2.11/dist/jquery.knob.min.js',
--- views/examples/dashboard/3.php
+++ views/examples/dashboard/3.php
@@ -1,11 +1,164 @@
 <?php
 
 use app\assets\widgets\dashboard\OnlineVisitorsAsset;
+use yii\web\View;
 
 $this->registerAssetBundle(OnlineVisitorsAsset::class);
 
 $this->title = Yii::t('app', 'Dashboard {number}', ['number' => 3]);
 $this->params['breadcrumbs'][] = $this->title;
+
+$JS = <<<JS
+$(function(){
+    'use strict';
+
+    var ticksStyle = {
+        fontColor: '#495057',
+        fontStyle: 'bold'
+    };
+
+    var mode = 'index';
+    var intersect = true;
+    var VisitorsChart = $('#visitors-chart');
+    var visitorsChart = new Chart(VisitorsChart, {
+        data: {
+            labels: ['18th', '20th', '22nd', '24th', '26th', '28th', '30th'],
+            datasets: [{
+                type: 'line',
+                data: [100, 120, 170, 167, 180, 177, 160],
+                backgroundColor: 'transparent',
+                borderColor: '#007bff',
+                pointBorderColor: '#007bff',
+                pointBackgroundColor: '#007bff',
+                fill: false
+                // pointHoverBackgroundColor: '#007bff',
+                // pointHoverBorderColor : '#007bff'
+            },
+                {
+                    type: 'line',
+                    data: [60, 80, 70, 67, 80, 77, 100],
+                    backgroundColor: 'tansparent',
+                    borderColor: '#ced4da',
+                    pointBorderColor: '#ced4da',
+                    pointBackgroundColor: '#ced4da',
+                    fill: false
+                    // pointHoverBackgroundColor: '#ced4da',
+                    // pointHoverBorderColor : '#ced4da'
+                }]
+        },
+        options: {
+            maintainAspectRatio: false,
+            tooltips: {
+                mode: mode,
+                intersect: intersect
+            },
+            hover: {
+                mode: mode,
+                intersect: intersect
+            },
+            legend: {
+                display: false
+            },
+            scales: {
+                yAxes: [{
+                    // display: false,
+                    gridLines: {
+                        display: true,
+                        lineWidth: '4px',
+                        color: 'rgba(0, 0, 0, .2)',
+                        zeroLineColor: 'transparent'
+                    },
+                    ticks: $.extend({
+                        beginAtZero: true,
+                        suggestedMax: 200
+                    }, ticksStyle)
+                }],
+                xAxes: [{
+                    display: true,
+                    gridLines: {
+                        display: false
+                    },
+                    ticks: ticksStyle
+                }]
+            }
+        }
+    }); 
+
+    var ticksStyle = {
+        fontColor: '#495057',
+        fontStyle: 'bold'
+    };
+
+    var mode = 'index';
+    var intersect = true;
+
+    var SalesChart = $('#sales-chart');
+    var salesChart = new Chart(SalesChart, {
+        type: 'bar',
+        data: {
+            labels: ['JUN', 'JUL', 'AUG', 'SEP', 'OCT', 'NOV', 'DEC'],
+            datasets: [
+                {
+                    backgroundColor: '#007bff',
+                    borderColor: '#007bff',
+                    data: [1000, 2000, 3000, 2500, 2700, 2500, 3000]
+                },
+                {
+                    backgroundColor: '#ced4da',
+                    borderColor: '#ced4da',
+                    data: [700, 1700, 2700, 2000, 1800, 1500, 2000]
+                }
+            ]
+        },
+        options: {
+            maintainAspectRatio: false,
+            tooltips: {
+                mode: mode,
+                intersect: intersect
+            },
+            hover: {
+                mode: mode,
+                intersect: intersect
+            },
+            legend: {
+                display: false
+            },
+            scales: {
+                yAxes: [{
+                    // display: false,
+                    gridLines: {
+                        display: true,
+                        lineWidth: '4px',
+                        color: 'rgba(0, 0, 0, .2)',
+                        zeroLineColor: 'transparent'
+                    },
+                    ticks: $.extend({
+                        beginAtZero: true,
+
+                        // Include a dollar sign in the ticks
+                        callback: function (value, index, values) {
+                            if (value >= 1000) {
+                                value /= 1000;
+                                value += 'k'
+                            }
+                            return '$' + value
+                        }
+                    }, ticksStyle)
+                }],
+                xAxes: [{
+                    display: true,
+                    gridLines: {
+                        display: false
+                    },
+                    ticks: ticksStyle
+                }]
+            }
+        }
+    })
+});
+JS;
+
+$this->registerJs($JS, View::POS_END);
 ?>
 <!DOCTYPE html>
 <html>
@@ -50,84 +203,6 @@ $this->params['breadcrumbs'][] = $this->title;
                         </div>
                     </div>
                 </div>
-                <script>
-                    $(function () {
-
-                        'use strict';
-
-                        var ticksStyle = {
-                            fontColor: '#495057',
-                            fontStyle: 'bold'
-                        };
-
-                        var mode = 'index';
-                        var intersect = true;
-                        var $visitorsChart = $('#visitors-chart');
-                        var visitorsChart = new Chart($visitorsChart, {
-                            data: {
-                                labels: ['18th', '20th', '22nd', '24th', '26th', '28th', '30th'],
-                                datasets: [{
-                                    type: 'line',
-                                    data: [100, 120, 170, 167, 180, 177, 160],
-                                    backgroundColor: 'transparent',
-                                    borderColor: '#007bff',
-                                    pointBorderColor: '#007bff',
-                                    pointBackgroundColor: '#007bff',
-                                    fill: false
-                                    // pointHoverBackgroundColor: '#007bff',
-                                    // pointHoverBorderColor : '#007bff'
-                                },
-                                    {
-                                        type: 'line',
-                                        data: [60, 80, 70, 67, 80, 77, 100],
-                                        backgroundColor: 'tansparent',
-                                        borderColor: '#ced4da',
-                                        pointBorderColor: '#ced4da',
-                                        pointBackgroundColor: '#ced4da',
-                                        fill: false
-                                        // pointHoverBackgroundColor: '#ced4da',
-                                        // pointHoverBorderColor : '#ced4da'
-                                    }]
-                            },
-                            options: {
-                                maintainAspectRatio: false,
-                                tooltips: {
-                                    mode: mode,
-                                    intersect: intersect
-                                },
-                                hover: {
-                                    mode: mode,
-                                    intersect: intersect
-                                },
-                                legend: {
-                                    display: false
-                                },
-                                scales: {
-                                    yAxes: [{
-                                        // display: false,
-                                        gridLines: {
-                                            display: true,
-                                            lineWidth: '4px',
-                                            color: 'rgba(0, 0, 0, .2)',
-                                            zeroLineColor: 'transparent'
-                                        },
-                                        ticks: $.extend({
-                                            beginAtZero: true,
-                                            suggestedMax: 200
-                                        }, ticksStyle)
-                                    }],
-                                    xAxes: [{
-                                        display: true,
-                                        gridLines: {
-                                            display: false
-                                        },
-                                        ticks: ticksStyle
-                                    }]
-                                }
-                            }
-                        })
-                    })
-                </script>
                 <!-- /.card -->
 
                 <div class="card">
@@ -275,84 +350,7 @@ $this->params['breadcrumbs'][] = $this->title;
                         </div>
                     </div>
                 </div>
-                <script>
-                    $(function () {
-
-                        'use strict';
-
-                        var ticksStyle = {
-                            fontColor: '#495057',
-                            fontStyle: 'bold'
-                        };
-
-                        var mode = 'index';
-                        var intersect = true;
-
-                        var $salesChart = $('#sales-chart');
-                        var salesChart = new Chart($salesChart, {
-                            type: 'bar',
-                            data: {
-                                labels: ['JUN', 'JUL', 'AUG', 'SEP', 'OCT', 'NOV', 'DEC'],
-                                datasets: [
-                                    {
-                                        backgroundColor: '#007bff',
-                                        borderColor: '#007bff',
-                                        data: [1000, 2000, 3000, 2500, 2700, 2500, 3000]
-                                    },
-                                    {
-                                        backgroundColor: '#ced4da',
-                                        borderColor: '#ced4da',
-                                        data: [700, 1700, 2700, 2000, 1800, 1500, 2000]
-                                    }
-                                ]
-                            },
-                            options: {
-                                maintainAspectRatio: false,
-                                tooltips: {
-                                    mode: mode,
-                                    intersect: intersect
-                                },
-                                hover: {
-                                    mode: mode,
-                                    intersect: intersect
-                                },
-                                legend: {
-                                    display: false
-                                },
-                                scales: {
-                                    yAxes: [{
-                                        // display: false,
-                                        gridLines: {
-                                            display: true,
-                                            lineWidth: '4px',
-                                            color: 'rgba(0, 0, 0, .2)',
-                                            zeroLineColor: 'transparent'
-                                        },
-                                        ticks: $.extend({
-                                            beginAtZero: true,
-
-                                            // Include a dollar sign in the ticks
-                                            callback: function (value, index, values) {
-                                                if (value >= 1000) {
-                                                    value /= 1000;
-                                                    value += 'k'
-                                                }
-                                                return '$' + value
-                                            }
-                                        }, ticksStyle)
-                                    }],
-                                    xAxes: [{
-                                        display: true,
-                                        gridLines: {
-                                            display: false
-                                        },
-                                        ticks: ticksStyle
-                                    }]
-                                }
-                            }
-                        })
-                    })
-                </script>
+            
                 <!-- /.card -->
 
                 <div class="card">
