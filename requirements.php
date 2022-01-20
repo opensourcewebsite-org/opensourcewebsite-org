874c88b5dea26784692d9e6ac74a36b70366c8cf
Vitaliy Shevchenko <grandmotivator@gmail.com>
2022-01-20 16:04:17 +0700

    configs

--- requirements.php
+++ requirements.php
@@ -57,10 +57,10 @@ if (extension_loaded('gd')) {
 }
 
 $connection = new \yii\db\Connection([
-    'dsn' => 'mysql:host=' . getenv('DB_HOST', 'localhost') . ';port=' . getenv('DB_PORT', '3306') . ';dbname=' . getenv('DB_NAME', 'opensourcewebsite'),
-    'username' => getenv('DB_USERNAME', 'root'),
-    'password' => getenv('DB_PASSWORD', ''),
-    'charset' => getenv('DB_CHARSET', 'utf8mb4'),
+    'dsn' => 'mysql:host=' . (getenv('DB_HOST') ?: 'localhost') . ';port=' . (getenv('DB_PORT') ?: '3306') . ';dbname=' . (getenv('DB_NAME') ?: 'opensourcewebsite'),
+    'username' => getenv('DB_USERNAME') ?: 'root',
+    'password' => getenv('DB_PASSWORD') ?: 'root',
+    'charset' => getenv('DB_CHARSET') ?: 'utf8mb4',
     'attributes' => [
         PDO::ATTR_CASE => PDO::CASE_LOWER,
     ],
