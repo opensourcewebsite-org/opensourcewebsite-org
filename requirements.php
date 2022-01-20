0b7c754618454a11fb8cc5db79802af67ea4f06b
Vitaliy Shevchenko <grandmotivator@gmail.com>
2022-01-20 11:39:29 +0700

    requirements.php

--- requirements.php
+++ requirements.php
@@ -9,9 +9,9 @@
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
 
-use Dotenv\Dotenv;
-
 require __DIR__ . '/vendor/autoload.php';
+
+use Dotenv\Dotenv;
 
 if (file_exists('.env')) {
     $dotenv = new Dotenv(__DIR__);
