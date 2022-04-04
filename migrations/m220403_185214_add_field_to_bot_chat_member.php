344f2ae7e2894bf1e4c8c5ad412b6a0299caabbc
Yurii Patrin <yury.patrin@gmail.com>
2022-04-04 13:17:25 +0300

    add FK and index

--- migrations/m220403_185214_add_field_to_bot_chat_member.php
+++ migrations/m220403_185214_add_field_to_bot_chat_member.php
@@ -12,7 +12,23 @@ class m220403_185214_add_field_to_bot_chat_member extends Migration
      */
     public function safeUp()
     {
-        $this->addColumn("{{%bot_chat_member}}", "invite_user_id", $this->integer()->null()->after("user_id"));
+        //add column
+        $this->addColumn("{{%bot_chat_member}}", "invite_user_id", $this->integer()->unsigned()->after("user_id"));
+
+        // creates index for column `user_id`
+        $this->createIndex(
+            'idx-bot_chat_member-invite_user_id',
+            '{{%bot_chat_member}}',
+            'invite_user_id'
+        );
+
+        //create foreign key
+        $this->addForeignKey(
+            'fk-bot_chat_member-invite_user_id',
+            '{{%bot_chat_member}}', 'invite_user_id',
+            '{{%bot_user}}', 'id',
+            'CASCADE', 'CASCADE'
+        );
     }
 
     /**
@@ -20,6 +36,16 @@ class m220403_185214_add_field_to_bot_chat_member extends Migration
      */
     public function safeDown()
     {
+        //drop FK
+        $this->dropForeignKey(
+            'fk-bot_chat_member-invite_user_id',
+            '{{%bot_chat_member}}'
+        );
+
+        //drop index
+        $this->dropIndex('idx-bot_chat_member-invite_user_id', '{{%bot_chat_member}}');
+
+        //drop column
         $this->dropColumn("{{%bot_chat_member}}", "invite_user_id");
     }
 
