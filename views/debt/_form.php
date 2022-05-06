a41a97173b49b093ded6310343c01613b1f85ab1
chozaf <92691728+chozaf@users.noreply.github.com>
2022-05-06 10:17:15 +0300

    select2 ajax pagination added for debt controller (#603)
    * select2 ajax pagination added for debt controller
    
    * select2 widget fix
    
    * restore lock file
    
    * created ContactSelect widget
    
    Co-authored-by: Дмитрий <dima@MacBook-Air-Dmitrij.local>

--- views/debt/_form.php
+++ views/debt/_form.php
@@ -4,13 +4,15 @@ use app\models\Debt;
 use app\widgets\buttons\CancelButton;
 use app\widgets\buttons\SaveButton;
 use yii\helpers\Html;
-use kartik\select2\Select2;
+use app\widgets\selects\ContactSelect;
+//use kartik\select2\Select2;
 use kartik\date\DatePicker;
 use yii\widgets\ActiveForm;
 use yii\helpers\ArrayHelper;
 use janisto\timepicker\TimePicker;
 use app\widgets\selects\CurrencySelect;
-
+use yii\web\JsExpression;
+use yii\helpers\Url;
 /* @var $this yii\web\View */
 /* @var $model app\models\Debt */
 /* @var $form yii\widgets\ActiveForm */
@@ -28,13 +30,9 @@ $form = ActiveForm::begin();
                         </div>
                     </div>
                     <div class="row">
-                        <div class="col">
-                            <?= $form->field($model, 'counter_user_id')->widget(Select2::class, [
-                                    'data' => ArrayHelper::map($users, 'id', 'displayName'),
-                                    'options' => [
-                                        'prompt' => 'Select...',
-                                    ],
-                                ]); ?>
+                        <div class="col">      
+                            <?=$form->field($model, 'counter_user_id')->widget(ContactSelect::class, ['pluginOptions' => ['ajax' => ['url' => Url::to(['ajax-users'])]]]);
+                            ?>
                         </div>
                     </div>
                     <div class="row">
