f83cee63122f23354b44ceca3209b310548ac57a
Dmytro <65899170+dimaBeldii@users.noreply.github.com>
2022-05-09 14:11:10 +0300

    Bugs and updates (#606)
    * Tasks from #html-ru
    
    * Tasks from #html-ru
    
    * Tasks from #html-ru
    
    * Tasks from #html-ru

--- views/vacancy/index.php
+++ views/vacancy/index.php
@@ -28,7 +28,7 @@ $displayActiveTab = $searchModel->status === VacancySearch::STATUS_ON;
 <div class="index">
     <div class="row">
         <div class="col-12">
-            <div class="card">
+            <div class="card table-overflow">
                 <div class="card-header d-flex p-0">
                     <div class="col-sm-6">
                         <ul class="nav nav-pills ml-auto p-2">
