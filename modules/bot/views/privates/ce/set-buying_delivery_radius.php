4916dcb6815afcb6ea793b1f4e597aca1d33603f
Pavel Yudin <pavel.yudin@gmail.com>
2022-09-09 17:19:49 +0300

    Fix CeController, add some features and changes

---
+++ modules/bot/views/privates/ce/set-buying_delivery_radius.php
@@ -0,0 +1,6 @@
+<b><?= Yii::t('bot', 'Send buying delivery radius, km') ?>.</b><br/>
+<br/>
+<i><?= Yii::t('bot', 'This information is used to find matches with offers from other users') ?>. <?= Yii::t('bot', 'Only you see this information') ?>.<br/>
+<br/>
+    - <?= Yii::t('bot', 'Your offer with a delivery radius sees other offers that contain a location or a delivery radius that intersect with your delivery radius') ?>.<br/>
+    - <?= Yii::t('bot', 'Your offer without a delivery radius sees other offers that contain a delivery radius that intersect with your location') ?>.</i>
