c3e3410ee610a315ec3d443cd8e3bacabf3038b6
LeChvor <68250014+LeChvor@users.noreply.github.com>
2022-12-26 14:21:16 +0200

    Fixes

--- config/db.php
+++ config/db.php
@@ -4,7 +4,7 @@ return [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=' . (getenv('DB_HOST') ?: 'localhost') . ';port=' . (getenv('DB_PORT') ?: '3306') . ';dbname=' . (getenv('DB_NAME') ?: 'opensourcewebsite'),
     'username' => getenv('DB_USERNAME') ?: 'root',
-    'password' => getenv('DB_PASSWORD'),
+    'password' => getenv('DB_PASSWORD') ?: 'root',
     'charset' => getenv('DB_CHARSET') ?: 'utf8mb4',
     'attributes' => [
         PDO::ATTR_CASE => PDO::CASE_LOWER,
