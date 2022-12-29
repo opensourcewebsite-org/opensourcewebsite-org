8ca84f49141c12fe103a2ee9f181a00d055d196d
Vitaliy Shevchenko <grandmotivator@gmail.com>
2022-12-29 12:21:44 +0700

    bot - group - mute/ban improve

--- modules/bot/controllers/groups/BanController.php
+++ modules/bot/controllers/groups/BanController.php
@@ -20,67 +20,63 @@ class BanController extends Controller
      */
     public function actionIndex()
     {
-        $telegramUser = $this->getTelegramUser();
+        $user = $this->getTelegramUser();
         $chat = $this->getTelegramChat();
 
         $chatMember = ChatMember::findOne([
             'chat_id' => $chat->id,
-            'user_id' => $telegramUser->id,
+            'user_id' => $user->id,
         ]);
 
         if ($chatMember->isActiveAdministrator()) {
-            if ($this->getMessage()->getText() !== null) {
-                if ($replyMessage = $this->getMessage()->getReplyToMessage()) {
-                    $replyUser = User::findOne([
-                        'provider_user_id' => $replyMessage->getFrom()->getId(),
+            if ($replyMessage = $this->getMessage()->getReplyToMessage()) {
+                $replyUser = User::findOne([
+                    'provider_user_id' => $replyMessage->getFrom()->getId(),
+                ]);
+
+                if ($replyUser) {
+                    $replyChatMember = ChatMember::findOne([
+                        'chat_id' => $chat->id,
+                        'user_id' => $replyUser->id,
                     ]);
-
-                    if ($replyUser) {
-                        $replyChatMember = ChatMember::findOne([
-                            'chat_id' => $chat->id,
-                            'user_id' => $replyUser->id,
-                        ]);
-                    }
-
-                    if (!isset($replyChatMember) || !$replyChatMember->isAdministrator()) {
-                        // delete replyMessage
-                        $this->getBotApi()->deleteMessage(
-                            $chat->getChatId(),
-                            $this->getMessage()->getReplyToMessage()->getMessageId()
-                        );
-
-                        // Kick member from the group
-                        $this->getBotApi()->kickChatMember(
-                            $chat->chat_id,
-                            $replyUser->provider_user_id
-                        );
-
-                        $replyUser->sendMessage(
-                            $this->render('/privates/warning-ban-chat-member', [
-                                'chat' => $chat,
-                            ]),
-                            [
-                                [
-                                    [
-                                        'callback_data' => GroupGuestController::createRoute('view', [
-                                            'id' => $chat->id,
-                                        ]),
-                                        'text' => Yii::t('bot', 'Group View'),
-                                    ],
-                                ],
-                            ]
-                        );
-                    }
-
                 }
 
-                // delete /ban command
-                $this->getBotApi()->deleteMessage(
-                    $chat->getChatId(),
-                    $this->getMessage()->getMessageId()
-                );
+                if (!isset($replyChatMember) || !$replyChatMember->isAdministrator()) {
+                    // delete replyMessage
+                    $this->getBotApi()->deleteMessage(
+                        $chat->getChatId(),
+                        $this->getMessage()->getReplyToMessage()->getMessageId()
+                    );
+                    // Kick member from the group
+                    $this->getBotApi()->kickChatMember(
+                        $chat->chat_id,
+                        $replyUser->provider_user_id
+                    );
+
+                    $replyUser->sendMessage(
+                        $this->render('/privates/warning-ban-chat-member', [
+                            'chat' => $chat,
+                        ]),
+                        [
+                            [
+                                [
+                                    'callback_data' => GroupGuestController::createRoute('view', [
+                                        'id' => $chat->id,
+                                    ]),
+                                    'text' => Yii::t('bot', 'Group View'),
+                                ],
+                            ],
+                        ]
+                    );
+                }
             }
         }
+
+        // delete /ban command
+        $this->getBotApi()->deleteMessage(
+            $chat->getChatId(),
+            $this->getMessage()->getMessageId()
+        );
 
         return [];
     }
--- modules/bot/controllers/groups/MuteController.php
+++ modules/bot/controllers/groups/MuteController.php
@@ -20,67 +20,63 @@ class MuteController extends Controller
      */
     public function actionIndex()
     {
-        $telegramUser = $this->getTelegramUser();
+        $user = $this->getTelegramUser();
         $chat = $this->getTelegramChat();
 
         $chatMember = ChatMember::findOne([
             'chat_id' => $chat->id,
-            'user_id' => $telegramUser->id,
+            'user_id' => $user->id,
         ]);
 
         if ($chatMember->isActiveAdministrator()) {
-            if ($this->getMessage()->getText() !== null) {
-                if ($replyMessage = $this->getMessage()->getReplyToMessage()) {
-                    $replyUser = User::findOne([
-                        'provider_user_id' => $replyMessage->getFrom()->getId(),
+            if ($replyMessage = $this->getMessage()->getReplyToMessage()) {
+                $replyUser = User::findOne([
+                    'provider_user_id' => $replyMessage->getFrom()->getId(),
+                ]);
+
+                if ($replyUser) {
+                    $replyChatMember = ChatMember::findOne([
+                        'chat_id' => $chat->id,
+                        'user_id' => $replyUser->id,
                     ]);
-
-                    if ($replyUser) {
-                        $replyChatMember = ChatMember::findOne([
-                            'chat_id' => $chat->id,
-                            'user_id' => $replyUser->id,
-                        ]);
-                    }
-
-                    if (!isset($replyChatMember) || !$replyChatMember->isAdministrator()) {
-                        // delete replyMessage
-                        $this->getBotApi()->deleteMessage(
-                            $chat->getChatId(),
-                            $this->getMessage()->getReplyToMessage()->getMessageId()
-                        );
-
-                        // Mute member of the group (currently forever)
-                        $this->getBotApi()->restrictChatMember(
-                            $chat->chat_id,
-                            $replyUser->provider_user_id
-                        );
-
-                        $replyUser->sendMessage(
-                            $this->render('/privates/warning-mute-chat-member', [
-                                'chat' => $chat,
-                            ]),
-                            [
-                                [
-                                    [
-                                        'callback_data' => GroupGuestController::createRoute('view', [
-                                            'id' => $chat->id,
-                                        ]),
-                                        'text' => Yii::t('bot', 'Group View'),
-                                    ],
-                                ],
-                            ]
-                        );
-                    }
-
                 }
 
-                // delete /mute command
-                $this->getBotApi()->deleteMessage(
-                    $chat->getChatId(),
-                    $this->getMessage()->getMessageId()
-                );
+                if (!isset($replyChatMember) || !$replyChatMember->isAdministrator()) {
+                    // delete replyMessage
+                    $this->getBotApi()->deleteMessage(
+                        $chat->getChatId(),
+                        $this->getMessage()->getReplyToMessage()->getMessageId()
+                    );
+                    // Mute member of the group (currently forever)
+                    $this->getBotApi()->restrictChatMember(
+                        $chat->chat_id,
+                        $replyUser->provider_user_id
+                    );
+
+                    $replyUser->sendMessage(
+                        $this->render('/privates/warning-mute-chat-member', [
+                            'chat' => $chat,
+                        ]),
+                        [
+                            [
+                                [
+                                    'callback_data' => GroupGuestController::createRoute('view', [
+                                        'id' => $chat->id,
+                                    ]),
+                                    'text' => Yii::t('bot', 'Group View'),
+                                ],
+                            ],
+                        ]
+                    );
+                }
             }
         }
+
+        // delete /mute command
+        $this->getBotApi()->deleteMessage(
+            $chat->getChatId(),
+            $this->getMessage()->getMessageId()
+        );
 
         return [];
     }
