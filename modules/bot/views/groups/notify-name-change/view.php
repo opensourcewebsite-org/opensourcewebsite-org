b39faa932a76a5c75b9fc15148929ebbb2ccbcfe
WinterSakuraa <darwinwoda@gmail.com>
2023-01-10 15:49:19 +0200

    bot - group - notify name change fix

--- modules/bot/views/groups/notify-name-change/view.php
+++ modules/bot/views/groups/notify-name-change/view.php
@@ -16,12 +16,8 @@ use app\modules\bot\components\helpers\Emoji;
 <br/>
 <b><?= Yii::t('bot', 'Telegram') ?> ID</b>: #<?= $user->getIdFullLink() ?><br/>
 <b><?= Yii::t('bot', 'Username') ?></b>: <?= $user->provider_user_name ? ' @' . $user->provider_user_name : '(No Username)' ?> <?= isset($changedAttributes->provider_user_name) ? Emoji::RIGHT . ' ' . $changedAttributes->provider_user_name : '' ?><br/>
-<?php if ($user->provider_user_first_name) : ?>
 <b><?= Yii::t('bot', 'First Name') ?></b>: <?= $user->provider_user_first_name ?> <?= isset($changedAttributes->provider_user_first_name) ? Emoji::RIGHT . ' ' .  $changedAttributes->provider_user_first_name : ' '; ?><br/>
-<?php endif; ?>
-<?php if ($user->provider_user_last_name) : ?>
 <b><?= Yii::t('bot', 'Last Name') ?></b>: <?= $user->provider_user_last_name ?> <?= isset($changedAttributes->provider_user_last_name) ? Emoji::RIGHT . ' ' .  $changedAttributes->provider_user_last_name : ' '; ?><br/>
-<?php endif; ?>
 <?php if ($globalUser = $user->globalUser) : ?>
 <br/>
 <b>OSW ID</b>: #<?= $globalUser->getIdFullLink() ?><?= ($globalUser->username ? ' @' . $globalUser->username : '') ?><br/>
