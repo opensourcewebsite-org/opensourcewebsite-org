1e8db00eb07fb52b8cac4a4d3d08dd1c1144eb45
WinterSakuraa <darwinwoda@gmail.com>
2023-01-28 17:39:33 +0200

    bot - group - view list feature

--- modules/bot/views/privates/group-faq/word-view-list.php
+++ modules/bot/views/privates/group-faq/word-view-list.php
@@ -1,7 +1,3 @@
-<b><?= $chat->title ?></b><br/>
-<br/>
-<?= Yii::t('bot', 'All questions') ?>:<br/>
-————————————————————<br/>
 <?php foreach($phrases as $phrase) : ?>
 <?= $phrase->text ?><br/>
 <?php endforeach; ?>
--- modules/bot/views/privates/group-message-filter/blacklist-word-view-list.php
+++ modules/bot/views/privates/group-message-filter/blacklist-word-view-list.php
@@ -1,7 +1,3 @@
-<b><?= $chat->title ?></b><br/>
-<br/>
-<?= Yii::t('bot', 'All blacklist phrases') ?>:<br/>
-————————————————————<br/>
 <?php foreach($phrases as $phrase) : ?>
 <?= $phrase->text ?><br/>
 <?php endforeach; ?>
