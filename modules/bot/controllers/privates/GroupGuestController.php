d33640e65e1cced62a63005e05967c0dce03ce2a
WinterSakuraa <darwinwoda@gmail.com>
2023-02-02 14:03:00 +0200

    bot - group guest - add remove membership

--- modules/bot/controllers/privates/GroupGuestController.php
+++ modules/bot/controllers/privates/GroupGuestController.php
@@ -43,6 +43,16 @@ class GroupGuestController extends Controller
         if ($chatMember) {
             $buttons[] = [
                 [
+                    'callback_data' => self::createRoute('remove-membership', [
+                        'id' => $chatMember->id,
+                    ]),
+                    'text' => Yii::t('bot', 'Remove membership'),
+                    'visible' => !$chatMember->checkMembership()
+                ],
+            ];
+
+            $buttons[] = [
+                [
                     'callback_data' => self::createRoute('input-intro-text', [
                         'id' => $chatMember->id,
                     ]),
@@ -489,6 +499,43 @@ class GroupGuestController extends Controller
                 $this->render('members-with-reviews'),
                 $buttons
             )
+            ->build();
+    }
+
+    /**
+     * @param int $id ChatMember->id
+     *
+     * @return array
+     */
+    public function actionRemoveMembership($id = null)
+    {
+        $chatMember = ChatMember::findOne($id);
+
+        if (isset($chatMember)) {
+            // remove membership
+            $chatMember->membership_date = null;
+            $chatMember->membership_note = null;
+            $chatMember->membership_tariff_price = null;
+            $chatMember->membership_tariff_days = null;
+
+
+            // remove slow mode
+            $chatMember->slow_mode_messages = 0;
+            $chatMember->slow_mode_messages_limit = null;
+            $chatMember->slow_mode_messages_skip_days = null;
+            $chatMember->slow_mode_messages_skip_hours = null;
+            $chatMember->last_message_at = null;
+
+            // remove limiter
+            $chatMember->limiter_date = null;
+
+            $chatMember->save();
+
+            return $this->actionView($chatMember->chat_id);
+        }
+
+        return $this->getResponseBuilder()
+            ->answerCallbackQuery()
             ->build();
     }
 }
