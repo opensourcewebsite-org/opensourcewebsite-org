86f89553e079c01efc2cc407853d47cf0f53eb84
WinterSakuraa <darwinwoda@gmail.com>
2023-02-03 13:52:10 +0200

    bot - group - /chat_id - add topic id if exist

--- modules/bot/views/groups/chat-id/index.php
+++ modules/bot/views/groups/chat-id/index.php
@@ -1,1 +1,4 @@
 <b><?= Yii::t('bot', 'Telegram') ?> Chat ID</b>: <?= $chat->getChatId() ?><br/>
+<?php if (isset($topicId)) : ?>
+<b><?= Yii::t('bot', 'Telegram') ?> Topic ID</b>: <?= $topicId ?><br/>
+<?php endif; ?>
