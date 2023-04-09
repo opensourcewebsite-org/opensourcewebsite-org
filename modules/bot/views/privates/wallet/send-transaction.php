b55493c59de8959c98bb6a6d9c81e17270455fdd
zubosem <zubosem@gmail.com>
2023-04-09 16:19:47 +0200

    fix

--- modules/bot/views/privates/wallet/send-transaction.php
+++ modules/bot/views/privates/wallet/send-transaction.php
@@ -1,10 +1,1 @@
-<?php
-
-use app\modules\bot\components\helpers\Emoji;
-
-?>
-
 <b><?= Yii::t('bot', 'Send @username or Telegram ID of the recipient') ?>:</b><br/>
-<?php if (isset($error)): ?>
-<br/><i><?=Emoji::WARNING?> <?= $error ?>.</i><br/>
-<?php endif; ?>
