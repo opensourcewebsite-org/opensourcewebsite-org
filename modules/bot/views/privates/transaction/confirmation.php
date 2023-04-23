1344437d511560cc4446170a13790125ba332edc
zubosem <zubosem@gmail.com>
2023-04-23 16:46:42 +0200

    fix spaces

--- modules/bot/views/privates/transaction/confirmation.php
+++ modules/bot/views/privates/transaction/confirmation.php
@@ -4,4 +4,3 @@
 <?= Yii::t('bot', 'Total amount') ?>: <?= $walletTransaction->getAmountPlusFee() ?> <?= $walletTransaction->currency->code ?><br/>
   • <?= Yii::t('bot', 'Amount') ?>: <?= $walletTransaction->amount ?> <?= $walletTransaction->currency->code ?><br/>
   • <?= Yii::t('bot', 'Fee') ?>: <?= $walletTransaction->fee ?> <?= $walletTransaction->currency->code ?><br/>
-  
