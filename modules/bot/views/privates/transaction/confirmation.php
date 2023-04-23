9a9bd5e707d768c7333feab61b449c4c41250488
zubosem <zubosem@gmail.com>
2023-04-23 16:44:19 +0200

    fix confirmation view

--- modules/bot/views/privates/transaction/confirmation.php
+++ modules/bot/views/privates/transaction/confirmation.php
@@ -1,19 +1,6 @@
-<?php
-
-use app\models\WalletTransaction;
-
-?>
-<?php if ($walletTransaction->amount) : ?>
 <b><?= Yii::t('bot', 'Transfer confirmation') ?></b><br/>
 <br/>
 <?= Yii::t('bot', 'Receiver') ?>: <?= $walletTransaction->toUser->botUser->getFullLink() ?><br/>
 <?= Yii::t('bot', 'Total amount') ?>: <?= $walletTransaction->getAmountPlusFee() ?> <?= $walletTransaction->currency->code ?><br/>
   • <?= Yii::t('bot', 'Amount') ?>: <?= $walletTransaction->amount ?> <?= $walletTransaction->currency->code ?><br/>
   • <?= Yii::t('bot', 'Fee') ?>: <?= $walletTransaction->fee ?> <?= $walletTransaction->currency->code ?><br/>
-<?php else : ?>
-<b><?= Yii::t('bot', 'Send amount for transfer') ?>:</b><br/>
-<br/>
-<i><?= Yii::t('bot', 'Available amount'); ?>: <?= $walletTransaction->fromUser->getWalletByCurrencyId(@$walletTransaction->currency->id)->getAmountMinusFee() . ' ' . @$walletTransaction->currency->code; ?></i><br/>
-————<br/>
-<?= Yii::t('bot', 'Receiver') ?>: <?= $walletTransaction->toUser->botUser->getFullLink() ?><br/>
-<?php endif; ?>
