287ee7594829bede1873cdf7bed4f45aeabfe4e9
zubosem <zubosem@gmail.com>
2023-04-28 11:27:54 +0200

    fix

--- modules/bot/views/privates/tip-queue/confirmation.php
+++ modules/bot/views/privates/tip-queue/confirmation.php
@@ -1,7 +1,8 @@
-<?php
-
-
-?>
-<b><?= Yii::t('bot', 'Tip confirmation') ?></b><br/>
+<b><?= Yii::t('bot', 'Multitip confirmation') ?></b><br/>
 <br/>
-
+<?= Yii::t('bot', 'Group') ?>: <b><?= $chatTipQueue->chat->title ?></b> <?= $chatTipQueue->chat->username ? ' (@' . $chatTipQueue->chat->username . ')' : '' ?>
+<br/>
+<?= Yii::t('bot', 'User amount') ?>: <?= $chatTipQueue->userAmount; ?> <?= $chatTipQueue->currency->code; ?>
+<br/>
+<?= Yii::t('bot', 'User count') ?>: <?= $chatTipQueue->userCount; ?>
+<br/>
