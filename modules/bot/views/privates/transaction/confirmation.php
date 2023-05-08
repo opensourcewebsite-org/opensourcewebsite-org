c95f32db84f4cfcfe094169187cf3910656adbfc
zubosem <zubosem@gmail.com>
2023-05-08 16:18:52 +0200

    Fix

--- modules/bot/views/privates/transaction/confirmation.php
+++ modules/bot/views/privates/transaction/confirmation.php
@@ -1,11 +1,17 @@
 <b><?= Yii::t('bot', 'Transfer confirmation') ?></b><br/>
-<br/>
-<?= Yii::t('bot', 'Receiver') ?>: <?= $walletTransaction->getReceiverLabel() ?><br/>
-<br/>
 <?php if ($walletTransaction->hasTypeLabel()): ?>
 <?= Yii::t('bot', 'Description') ?>: <?= $walletTransaction->getTypeLabel() ?><br/>
 <br/>
 <?php endif; ?>
+<?php if (!$walletTransaction->anonymity): ?>
+<br/>
+<?= Yii::t('bot', 'Receiver') ?>: <?= $walletTransaction->getReceiverLabel() ?><br/>
+<?php endif; ?>
+<?php if ($walletTransaction->hasGroupLabel()): ?>
+<br/>
+<?= Yii::t('bot', 'Group') ?>: <?= $walletTransaction->getGroupLabel() ?><br/>
+<?php endif; ?>
+<br/>
 <b><?= Yii::t('bot', 'Total amount') ?>: <?= $walletTransaction->getAmountPlusFee() ?> <?= $walletTransaction->currency->code ?></b><br/>
   • <?= Yii::t('bot', 'Amount') ?>: <?= $walletTransaction->amount ?> <?= $walletTransaction->currency->code ?><br/>
   • <?= Yii::t('bot', 'Fee') ?>: <?= $walletTransaction->fee ?> <?= $walletTransaction->currency->code ?><br/>
--- modules/bot/views/privates/wallet/transaction.php
+++ modules/bot/views/privates/wallet/transaction.php
@@ -1,18 +1,20 @@
-<?php
-
-use app\models\WalletTransaction;
-
-?>
 <b><?= ($user->getUserId() == $walletTransaction->getFromUserID() ? '-' : '+') . $walletTransaction->getAmount() . ' ' . $walletTransaction->currency->code ?> - <?= Yii::$app->formatter->asDateTime($walletTransaction->getCreatedAtByUser()); ?></b><br/>
-<br/>
-<?= Yii::t('bot', 'Sender') ?>: <?= $walletTransaction->fromUser->botUser->getFullLink() ?><br/>
-<br/>
-<?= Yii::t('bot', 'Receiver') ?>: <?= $walletTransaction->getReceiverLabel() ?><br/>
 <?php if ($walletTransaction->hasTypeLabel()) : ?>
 <br/>
 <?= Yii::t('bot', 'Description') ?>: <?= $walletTransaction->getTypeLabel() ?><br/>
+<?php endif; ?>
+<br/>
+<?= Yii::t('bot', 'Sender') ?>: <?= $walletTransaction->fromUser->botUser->getFullLink() ?><br/>
+<?php if (!$walletTransaction->anonymity): ?>
+<br/>
+<?= Yii::t('bot', 'Receiver') ?>: <?= $walletTransaction->getReceiverLabel() ?><br/>
+<?php endif; ?>
+<?php if ($walletTransaction->hasGroupLabel()): ?>
+<br/>
+<?= Yii::t('bot', 'Group') ?>: <?= $walletTransaction->getGroupLabel() ?><br/>
 <?php endif; ?>
 <?php if ($user->getUserId() == $walletTransaction->getFromUserID()) : ?>
 <br/>
 <?= Yii::t('bot', 'Fee') ?>: <?= $walletTransaction->fee ?> <?= $walletTransaction->currency->code ?><br/>
 <?php endif; ?>
+    
