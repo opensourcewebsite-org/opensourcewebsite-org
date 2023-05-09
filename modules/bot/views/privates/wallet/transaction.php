a54cdddaba851b14dc694a7ff49973c17c9b54af
zubosem <91156513+zubosem@users.noreply.github.com>
2023-05-09 05:26:18 +0200

    Fix views (#707)
    * fix
    
    * drop private_message_ids after deleting real messages
    add full wallet amount when input amount Money step
    
    * fix
    
    * WalletTest, Install.md, Contributing.md modify
    
    * fix
    
    * remove unused translates
    
    * add rule single_space_around_construct to cs-fixer
    
    * fix
    
    * fix
    
    * fix composer, fix syntax, remove unnecessary operation
    
    * fix
    
    * fix
    
    * fix
    
    * Send tip + Send money feature
    Comfortable backward logic
    
    * save state when chat is private
    
    * fix state saving bug
    make valid response when /tip by button
    fix receiver and sender names in payment process
    
    * fix
    
    * fix
    
    * UserState upgrade
    
    * - create TransactionController
    - backward routing feature with storing route in UserState
    - remove transaction functional from WalletController
    
    * Fix problem with getting UserState
    
    * Fix problem with getting UserState
    
    * fix confirmation view
    
    * fix
    
    * fix spaces
    
    * fix
    
    * fix getting and setting input route
    
    * Send tip to anonymous admin
    
    * - Redirect anonymous tip to GroupGuestController
    - Hide receiver from views
    - Add description depends of transaction type
    - Add type label to WalletTransaction
    - Add translations to russian
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * Fix
    
    * Fix
    
    * fix
    
    * fix

--- modules/bot/views/privates/wallet/transaction.php
+++ modules/bot/views/privates/wallet/transaction.php
@@ -1,16 +1,17 @@
-<?php
-
-use app\models\WalletTransaction;
-
-?>
 <b><?= ($user->getUserId() == $walletTransaction->getFromUserID() ? '-' : '+') . $walletTransaction->getAmount() . ' ' . $walletTransaction->currency->code ?> - <?= Yii::$app->formatter->asDateTime($walletTransaction->getCreatedAtByUser()); ?></b><br/>
-<br/>
-<?= Yii::t('bot', 'Sender') ?>: <?= $walletTransaction->fromUser->botUser->getFullLink() ?><br/>
-<br/>
-<?= Yii::t('bot', 'Receiver') ?>: <?= $walletTransaction->getReceiverLabel() ?><br/>
 <?php if ($walletTransaction->hasTypeLabel()) : ?>
 <br/>
 <?= Yii::t('bot', 'Description') ?>: <?= $walletTransaction->getTypeLabel() ?><br/>
+<?php endif; ?>
+<br/>
+<?= Yii::t('bot', 'Sender') ?>: <?= $walletTransaction->fromUser->botUser->getFullLink() ?><br/>
+<?php if (!$walletTransaction->anonymity): ?>
+<br/>
+<?= Yii::t('bot', 'Receiver') ?>: <?= $walletTransaction->getReceiverLabel() ?><br/>
+<?php endif; ?>
+<?php if ($walletTransaction->hasGroupLabel()): ?>
+<br/>
+<?= Yii::t('bot', 'Group') ?>: <?= $walletTransaction->getGroupLabel() ?><br/>
 <?php endif; ?>
 <?php if ($user->getUserId() == $walletTransaction->getFromUserID()) : ?>
 <br/>
